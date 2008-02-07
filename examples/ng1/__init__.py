0650a55ec0317301101dfc28924988e002e7cb18
pkienzle <pkienzle@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2008-02-07 22:52:48 +0000

    Sample data for NG1 and NG7
    git-svn-id: svn://danse.us/reflectometry/trunk/reflectometry/reduction@451 a92bf6fb-5b16-0410-9b54-b8becc4ab72c

---
+++ examples/ng1/__init__.py
@@ -0,0 +1,36 @@
+"""
+Sample data from NCNR-NG1.
+
+This is data from March, 2002.
+
+Note that this data has a few problems with it such as internal name not
+matching external name, wavelength being incorrect and slits scans being
+saturated.
+
+TODO: Ask Ursula Perez-Salas what this data contains.
+"""
+
+import numpy, os
+import reflectometry.reduction as reflred
+
+PATH = os.path.dirname(os.path.realpath(__file__))
+
+def load(seqlist,base='psih1'):
+    return [reflred.load_ng1(os.path.join(PATH,"%s%03d.ng1"%(base,seq)))
+            for seq in seqlist]
+
+def slits():
+    return load([6,7,8,9,10,11],base='ibeam')
+
+def back():
+    return load([9,10,11,12,13,15,16,17,18,19])
+
+def spec():
+    return load([2,3,4,6,7])
+
+def rock():
+    return load([1,5,8])
+
+
+if __name__ == "__main__":
+    print spec()[0]
---
+++ examples/ng7/__init__.py
@@ -0,0 +1,33 @@
+"""
+Sample data from NCNR-NG7.
+
+This data from July, 2004, measures water with various concentrations
+for D2O on quartz.  The particular data sets contain 20% D2O.
+
+TODO: Confirm this is correct by looking in log books!
+"""
+
+import numpy, os
+import reflectometry.reduction as reflred
+
+PATH = os.path.dirname(os.path.realpath(__file__))
+
+def load(seqlist,base='jul04'):
+    return [reflred.load_ng7(os.path.join(PATH,"%s%03d.ng7"%(base,seq)))
+            for seq in seqlist]
+
+def slits():
+    return load('ibeam',[36])
+
+def back():
+    return load([31,32])
+
+def spec():
+    return load([37])
+
+def rock():
+    return load([33,34,35])
+
+
+if __name__ == "__main__":
+    print spec()[0]
