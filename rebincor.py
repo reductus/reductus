57c218850e6070907bf8fa320017184a33790b5c
pkienzle <pkienzle@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2008-02-13 15:28:31 +0000

    Minor doc changes
    git-svn-id: svn://danse.us/reflectometry/trunk/reflectometry/reduction@468 a92bf6fb-5b16-0410-9b54-b8becc4ab72c

--- rebincor.py
+++ rebincor.py
@@ -4,14 +4,22 @@ class LinearBinning(object):
     """
     Desired time binning for the dataset.
     
-    start (1 Angstrom)
+    start (0 Angstrom)
     stop (inf Angstrom)
     step (0.1 Angstrom)
+    
+    Note that the limit will automatically cut off at the time bin
+    boundaries, so can be infinite.
     """
-    start = 1.
+    start = 0
     stop = inf
-    step = None
-    def __init__(self, **kw): _set(self,kw)
+    step = 0.1
+    def __init__(self, start=0.,stop=inf,step=0.1):
+        self.start,self.step,self.stop = start,step,stop
+
+    def __str__(self):
+        return "LinearBinning(start=%g,stop=%g,step=%g)"\
+            %(self.start,self.stop,self.step)
 
 class LogBinning(object):
     """
@@ -20,8 +28,16 @@ class LogBinning(object):
     start (1 Angstrom)
     stop (inf Angstrom)
     step (1 %)
+
+    Note that the upper limit will automatically cut off at the maximum
+    time bin boundary, so it can be infinite.
     """
-    start = 1.
-    stop = inf
-    step = 1.
-    def __init__(self, **kw): _set(self,kw)
+    start = 1
+    stop = 10
+    step = 1
+    def __init__(self, start=1.,stop=inf,step=1):
+        self.start,self.step,self.stop = start,step,stop
+
+    def __str__(self):
+        return "LogBinning(start=%g,stop=%g,step=%g)"\
+            %(self.start,self.stop,self.step)
