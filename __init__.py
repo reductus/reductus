a993b864e71a7763067bb7233c1d0c3e065ba74b
pkienzle <pkienzle@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2008-02-13 14:06:47 +0000

    Move Q calculation out of data loader
    git-svn-id: svn://danse.us/reflectometry/trunk/reflectometry/reduction@467 a92bf6fb-5b16-0410-9b54-b8becc4ab72c

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
 # This code is in the public domain
 
 from polcor import *
+from normalize import *
 
 def load_ng7(file):
     "NCNR NG-7 file loader"
     from .ncnr_ng7 import NG7Icp
     data = NG7Icp(file)
     data.load()
+    data.resetQ()
     return data
 
 def load_ng1(file):
@@ -14,4 +16,5 @@ def load_ng1(file):
     from .ncnr_ng1 import NG1Icp
     data = NG1Icp(file)
     data.load()
+    data.resetQ()
     return data
