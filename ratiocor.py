99ad6eb762bc5239dd8a95d31f37ac73d5b5781c
pkienzle <pkienzle@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2008-02-14 19:17:10 +0000

    Rewrite to use Fresnel
    git-svn-id: svn://danse.us/reflectometry/trunk/reflectometry/reduction@475 a92bf6fb-5b16-0410-9b54-b8becc4ab72c

--- ratiocor.py
+++ ratiocor.py
@@ -32,23 +32,24 @@ if __name__ == '__main__':
 
 from math import *
 import reflectometry.model1d as reflfit
+from .fresnel import Fresnel
+import numpy
 
 class RatioCorrection(object):
     model = None
-    name = None
     
-    def __init__(self, model, name=None):
-        if name: self.name = name
+    def __init__(self, model):
         self.model = model
-        
+
     def __call__(self, data):
         intensity_ratio(data=data,model=self.model)
+        data.log(str(self))
         return data
-    
-    def __str__(self):
-        return self.name if name else "RatioCorrection"
 
-def water_ratio(D2O=0,probe=None,**kw):
+    def __str__(self):
+        return "RatioCorrection(%s)"%str(model)
+
+class WaterRatio(object):
     """
     Compute the incident intensity assuming that data measures a 
     reflection off water.
@@ -58,45 +59,79 @@ def water_ratio(D2O=0,probe=None,**kw):
 
     Returns a correction object that can be applied to data.    
     """
-    # SLDs calculated with NBCU.XLS from the NCNR website.
-    if probe == 'neutron':
-        rho_H2O,mu_H2O = -0.560, 0.001
-        rho_D2O,mu_D2O = 5.756, 0.00
-    elif probe == 'xray':
-        rho_H2O,mu_H2O = 9.466, 0.098
-        rho_D2O,mu_D2O = 8.516, 0.089
-    else:
-        raise ValueError, "water_ratio needs probe 'neutron' or 'xray'"
-    rho = ((100-D2O)*(rho_H2O) + D2O*rho_H2O)/100.
-    mu = ((100-D2O)*(mu_H2O) + D2O*mu_H2O)/100.
-    m = reflfit.Model()
-    m.incident('Air')
-    m.interface(sigma=3)
-    m.substrate('water', rho=rho, mu=mu)
-    name = "RatioCorrection(water,D2O=%d)"%D2O
-    return RatioCorrection(m, name)
-    
+    def _set_fresnel_coefficients(self):
+        # SLDs calculated with NBCU.XLS from the NCNR website.
+        if self.probe == 'neutron':
+            rho_H2O,mu_H2O = -0.560, 0.001
+            rho_D2O,mu_D2O = 5.756, 0.00
+        elif self.probe == 'xray':
+            rho_H2O,mu_H2O = 9.466, 0.098
+            rho_D2O,mu_D2O = 8.516, 0.089
+        else:
+            raise ValueError, "water_ratio needs probe 'neutron' or 'xray'"
+        D2O = self._D2O
+        self.model.rho = ((100-D2O)*(rho_H2O) + D2O*rho_H2O)/100.
+        self.model.mu = ((100-D2O)*(mu_H2O) + D2O*mu_H2O)/100.
+        
+    def _setD2O(self, D2O):
+        self._D2O = D2O
+        self._set_fresnel_coefficients
+    def _getD2O(self):
+        return self._D2O
+    def _setprobe(self,probe):
+        self._probe = probe
+        self._set_fresnel_coefficients
+    def _getprobe(self):
+        return self._probe
+    D2O = property(_getD2O,_setD2O)
+    probe = property(_getprobe,_setprobe)
+
+    def __init__(self,D2O=0,probe=None):
+        self.model = Fresnel(sigma=3)
+        self._probe = probe
+        self._D2O = D2O
+        self._set_fresnel_coefficients()
+
+    def __call__(self,data):
+        intensity_ratio(data=data,model=self.model)
+        data.log(str(self))
+        return data
+
+    def __str__(self):
+        return "WaterRatio(D2O=%d,probe=%s)"%(self._D2O,self._probe)
+
 def intensity_ratio(data=None,model=None):
     """
     Compute the incident intensity assuming that data measures a 
     reflection off the given model.
-    
+
     Returns a copy of the dataset that can be subsequently written
     to a data file for use in reflpak or other reduction software.
     """
-    Q,dQ,L = data.Q,data.dQ,data.wavelength
+
+    # TODO for now we are not using resolution information when
+    # computing the data/theory ratio
+    Q,L = data.Qz,data.detector.wavelength
+    R = model.reflectivity(Q,L)
+    data.R, data.dR = data.R/R, data.dR/R
+    data.intent = 'intensity'
+    return
 
     # Compute the convolved reflectivity.
     # Find the theory points necessary to compute an accurate estimate of
     # the convolved model without over- or under-sampling.
-    Qi = model.resample_Q(Q,dQ)
+    Q,dQ,L = data.Qz,data.dQ,data.wavelength
+    Qi = model.fitQ(Q,dQ)
     Ri = model.reflectivity(Qi,L)
     R = reflfit.convolve(Qi,Ri,Q,dQ)
     data.R, data.dR = data.R/R, data.dR/R
 
 def demo():
     from .examples import ng7 as dataset
+    from .normalize import Normalize
     spec = dataset.spec()[0]
-    spec.apply(water_ratio(D2O=20,probe=spec.probe))
-    
+    spec.apply(Normalize())
+    spec.apply(WaterRatio(D2O=20,probe=spec.probe))
+    print spec
+
 if __name__ == "__main__": demo()
