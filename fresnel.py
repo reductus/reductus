168bb3ec4a0f6ce926351f811144ded840349266
pkienzle <pkienzle@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2008-03-18 14:10:57 +0000

    use model terminology consistent with model1d
    git-svn-id: svn://danse.us/reflectometry/trunk/reflectometry/reduction@566 a92bf6fb-5b16-0410-9b54-b8becc4ab72c

--- fresnel.py
+++ fresnel.py
@@ -9,30 +9,36 @@ from numpy import sqrt, exp, real, conj, pi, log, abs, choose
 class Fresnel:
     """
     Function for computing the Fresnel reflectivity for a single interface.
+    
+    rho,Vrho
+        scattering length density of substrate and vacuum
+    mu,Vmu
+        absorption of substrate and vacuum (note that we do not correct
+        for attenuation of the beam through the incident medium since we
+        do not know the path length).
+    sigma (angstrom)
+        interfacial roughness
     """
-    def __init__(self, rho=1e-6, Vrho=0, mu=0, sigma=0):
-        self.rho,self.Vrho,self.mu,self.sigma = rho,Vrho,mu,sigma
+    def __init__(self, rho=1e-6, mu=0, sigma=0, Vrho=0, Vmu=0):
+        self.rho,self.Vrho,self.mu,self.Vmu,self.sigma = rho,Vrho,mu,Vmu,sigma
 
     def reflectivity(self, Q, L=1):
         """
         Compute the Fresnel reflectivity at the given Q/wavelength.
         """
-        rho_s = self.rho    # substrate density
-        rho_v = self.Vrho   # incident medium density
-        mu_s = self.mu      # absorption
-        sigma = self.sigma  # roughness
         # If Q < 0, then we are going from substrate into incident medium.
         # In that case we must negate the change in scattering length density
         # and ignore the absorption.
-        S = 16*pi*(rho_s-rho_v)*choose(Q<0,(-1,1)) \
-            + 8j*pi*mu_s/L*choose(Q<0,(0,1))
-        Q = abs(Q)
-        f = sqrt(Q**2 - S)  # fresnel coefficient
+        drho = self.rho-self.Vrho
+        S = 4*pi*choose(Q<0,(-drho,drho)) \
+            + 2j*pi/L*choose(Q<0,(self.Vmu,self.mu))
+        kz = abs(Q)/2
+        f = sqrt(kz**2 - S)  # fresnel coefficient
 
         # Compute reflectivity amplitude, with adjustment for roughness
-        amp = (Q-f)/(Q+f) * exp(-0.5*self.sigma**2*Q*f)
+        amp = (kz-f)/(kz+f) * exp(-2*self.sigma**2*kz*f)
         # Note: we do not need to check for a divide by zero.
-        # Let Qc^2 = 16 pi rho.  Since rho is non-zero then Qc is non-zero.
+        # Qc^2 = 16 pi rho.  Since rho is non-zero then Qc is non-zero.
         # For mu = 0:
         # * If |Qz| < Qc then f has an imaginary component, so |Qz|+f != 0.
         # * If |Qz| > Qc then |Qz| > 0 and f > 0, so |Qz|+f != 0.
