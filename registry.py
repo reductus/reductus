986a9fedfb7f1976ff2df6ca1e9e4caf1d8d1738
pkienzle <pkienzle@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2008-07-10 20:21:31 +0000

    reduction: registry supports extra '.' in file names
    git-svn-id: svn://danse.us/reflectometry/trunk/reflectometry/reduction@748 a92bf6fb-5b16-0410-9b54-b8becc4ab72c

--- registry.py
+++ registry.py
@@ -20,18 +20,26 @@ class ExtensionRegistry(object):
 
     # Add an association by setting an element
     registry['.zip'] = unzip
-
+    
     # Multiple extensions for one loader
     registry['.tgz'] = untar
     registry['.tar.gz'] = untar
+
+    # Generic extensions to use after trying more specific extensions; 
+    # these will be checked after the more specific extensions fail.
+    registry['.gz'] = gunzip
 
     # Multiple loaders for one extension
     registry['.cx'] = cx1
     registry['.cx'] = cx2
     registry['.cx'] = cx3
+
+    # Show registered extensions
+    print registry.extensions()
     
     # Can also register a format name for explicit control from caller
     registry['cx3'] = cx3
+    print registry.formats()
 
     # Retrieve loaders for a file name
     registry.lookup('hello.cx') -> [cx3,cx2,cx1]
@@ -60,38 +68,52 @@ class ExtensionRegistry(object):
         return self.loaders[ext]
     def __contains__(self, ext):
         return ext in self.loaders
-    def formats(self, name=True, ext=False):
+    def formats(self):
         """
-        Return a list of the registered formats.  If name=True then
-        named formats are returned.  If ext=True then extensions
-        are returned.
+        Return a sorted list of the registered formats.
         """
         names = [a for a in self.loaders.keys() if not a.startswith('.')]
-        exts = [a for a in self.loaders.keys() if a.startswith('.')]
         names.sort()
+        return names
+    def extensions(self):
+        """
+        Return a sorted list of registered extensions.
+        """
+        exts = [a for a in self.loaders.keys() if a.startswith('.')]
         exts.sort()
-        ret = []
-        if name: ret += names
-        if ext: ret += exts
-        return ret
-        
+        return exts
     def lookup(self, path):
         """
         Return the loader associated with the file type of path.
+        
+        Raises ValueError if file type is not known.
         """        
-        file = os.path.basename(path)
-        idx = file.find('.')
-        ext = file[idx:] if idx >= 0 else ''
-        try:
-            return self.loaders[ext]
-        except:
-            raise ValueError, "Unknown file type '%s'"%ext
+        # Find matching extensions
+        extlist = [ext for ext in self.extensions() if path.endswith(ext)]
+        # Sort matching extensions by decreasing order of length
+        extlist.sort(lambda a,b: len(a)<len(b))
+        # Combine loaders for matching extensions into one big list
+        loaders = []
+        for L in [self.loaders[ext] for ext in extlist]:
+            loaders.extend(L)
+        # Remove duplicates if they exist
+        if len(loaders) != len(set(loaders)):
+            result = []
+            for L in loaders:
+                if L not in result: result.append(L)
+            loaders = L
+        # Raise an error if there are no matching extensions
+        if len(loaders) == 0:
+            raise ValueError, "Unknown file type for "+path
+        # All done
+        return loaders
     def load(self, path, format=None):
         """
         Call the loader for the file type of path.
 
         Raises ValueError if no loader is available.
-        May raise a loader-defined exception if loader fails.
+        Raises KeyError if format is not available.
+        May raise a loader-defined exception if loader fails.        
         """
         if format is None:
             loaders = self.lookup(path)
@@ -111,10 +133,14 @@ def test():
     def cx(file): return 'cx'
     def new_cx(file): return 'new_cx'
     def fail_cx(file): raise CxError
+    def cat(file): return 'cat'
+    def gunzip(file): return 'gunzip'
     reg['.cx'] = cx
     reg['.cx1'] = cx
     reg['.cx'] = new_cx
+    reg['.gz'] = gunzip
     reg['.cx.gz'] = new_cx
+    reg['.cx1.gz'] = fail_cx
     reg['.cx1'] = fail_cx
     reg['.cx2'] = fail_cx
     reg['new_cx'] = new_cx
@@ -136,9 +162,13 @@ def test():
     # Make sure the case of no loaders fails correctly
     try: reg.load('hello.missing')
     except ValueError,msg:
-        assert str(msg)=="Unknown file type '.missing'",'Message: <%s>'%(msg)
+        assert str(msg)=="Unknown file type for hello.missing",'Message: <%s>'%(msg)
     else: raise AssertError,"No error raised for missing extension"
     assert reg.formats() == ['new_cx']
-    assert reg.formats(name=False,ext=True) == ['.cx','.cx.gz','.cx1','.cx2']
+    assert reg.extensions() == ['.cx','.cx.gz','.cx1','.cx1.gz','.cx2','.gz']
+    # make sure that it supports multiple '.' in filename
+    assert reg.load('hello.extra.cx1') == 'cx'
+    assert reg.load('hello.gz') == 'gunzip'
+    assert reg.load('hello.cx1.gz') == 'gunzip' # Since .cx1.gz fails
 
 if __name__ == "__main__": test()
