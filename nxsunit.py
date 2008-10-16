7600c03cd6686e069416b2306d69086a5020f6d4
pkienzle <pkienzle@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2008-10-16 18:43:58 +0000

    add a.u. for arbitrary units
    git-svn-id: svn://danse.us/reflectometry/trunk/reflectometry/reduction@804 a92bf6fb-5b16-0410-9b54-b8becc4ab72c

--- nxsunit.py
+++ nxsunit.py
@@ -36,12 +36,19 @@ NeXus example:
 This is a standalone module, not relying on either DANSE or NeXus, and
 can be used for other unit conversion tasks.
 
+Note: minutes are used for angle and seconds are used for time.  We
+cannot tell what the correct interpretation is without knowing something
+about the fields themselves.  If this becomes an issue, we will need to
+allow the application to set the dimension for the units rather than
+getting the dimension from the units as we are currently doing.
 """
 
 # TODO: Add udunits to NAPI rather than reimplementing it in python
 # TODO: Alternatively, parse the udunits database directly
 # UDUnits:
 #  http://www.unidata.ucar.edu/software/udunits/udunits-1/udunits.txt
+
+# TODO: Allow application to impose the map on the units
 
 from __future__ import division
 import math
@@ -89,16 +96,19 @@ def _build_plural_units(**kw):
     return map
 
 def _build_all_units():
+    # Various distance measures
     distance = _build_metric_units('meter','m')
     distance.update(_build_metric_units('metre','m'))
     distance.update(_build_plural_units(micron=1e-6, Angstrom=1e-10))
     distance.update({'A':1e-10, 'Ang':1e-10})
 
+    # Various time measures.
+    # Note: minutes are used for angle rather than time
     time = _build_metric_units('second','s')
-    # Note: minutes are used for angle
     time.update(_build_plural_units(hour=3600,day=24*3600,week=7*24*3600))
 
-    # Note: seconds are used for time
+    # Various angle measures.
+    # Note: seconds are used for time rather than angle
     angle = _build_plural_units(degree=1, minute=1/60.,
                   arcminute=1/60., arcsecond=1/3600., radian=180/math.pi)
     angle.update(deg=1, arcmin=1/60., arcsec=1/3600., rad=180/math.pi)
@@ -119,8 +129,10 @@ def _build_all_units():
     Q = { 'invAng': 1, 'invAngstroms': 1,
           '10^-3 Angstrom^-1': 1e-3, 'nm^-1': 10 }
 
-    unknown = {None:1, '???':1, '': 1}
-
+    # APS files may be using 'a.u.' for 'arbitrary units'.  Other
+    # facilities are leaving the units blank, using ??? or not even
+    # writing the units attributes.
+    unknown = {None:1, '???':1, '': 1, 'a.u.':1}
 
     dims = [unknown, distance, time, angle, frequency,
             temperature, charge, sld, Q]
@@ -142,10 +154,11 @@ class Converter(object):
             if name in map:
                 self.scalemap = map
                 self.scalebase = self.scalemap[name]
-                return
-        self.scalemap = {'': 1}
-        self.scalebase = 1
-        #raise ValueError, "Unknown unit %s"%name
+                break
+        else:
+            self.scalemap = {'': 1}
+            self.scalebase = 1
+            #raise ValueError, "Unknown unit %s"%name
 
     def scale(self, units=""):
         if units == "" or self.scalemap is None: return 1
@@ -165,6 +178,7 @@ class Converter(object):
 
 def _check(expect,get):
     if expect != get: raise ValueError, "Expected %s but got %s"%(expect,get)
+    #print expect,"==",get
 
 def test():
     _check(2,Converter('mm')(2000,'m')) # 2000 mm -> 2 m
@@ -172,6 +186,7 @@ def test():
     _check(45,Converter('nanokelvin')(45))  # 45 nK -> 45 nK
     # TODO: more tests
     _check(0.5,Converter('seconds')(1800,units='hours')) # 1800 -> 0.5 hr
+    _check(2.5,Converter('a.u.')(2.5,units=''))
 
 if __name__ == "__main__":
     test()
