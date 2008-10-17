cd8059cb66182b5a1b84284df4c1bce1ccbcfeb5
pkienzle <pkienzle@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2008-10-17 21:09:16 +0000

    reduction: wsolve now implements confidence intervals
    git-svn-id: svn://danse.us/reflectometry/trunk/reflectometry/reduction@809 a92bf6fb-5b16-0410-9b54-b8becc4ab72c

--- wsolve.py
+++ wsolve.py
@@ -8,26 +8,30 @@ Use:   s = wsolve(A,y,dy)
 wsolve uses the singular value decomposition for increased accuracy.
 Estimates the uncertainty for the solution from the scatter in the data.
 
-The returned object s provides:
+The returned model object s provides:
 
     s.x      solution
     s.std    uncertainty estimate assuming no correlation
     s.rnorm  residual norm
     s.DoF    degrees of freedom
     s.cov    covariance matrix
-    s.ci(x)  confidence intervals computed at x
-    s.pi(x)  prediction intervals computed at x
+    s.ci(p)  confidence intervals at point p
+    s.pi(p)  prediction intervals at point p
+    s(p)     predicted value at point p
 
-Example: weighted system
+Example
+=======
 
-import numpy,wsolve
-A = numpy.matrix("1,2,3;2,1,3;1,1,1",'d').A
-xin = numpy.array([1,2,3],'d')
-dy = numpy.array([0.2,0.01,0.1])
-y = numpy.random.normal(numpy.dot(A,xin),dy)
-print A,y,dy
-s = wsolve.wsolve(A,y,dy)
-print "xin,x,dx", xin, s.x, s.std
+Weighted system::
+
+    import numpy,wsolve
+    A = numpy.matrix("1,2,3;2,1,3;1,1,1",'d').A
+    xin = numpy.array([1,2,3],'d')
+    dy = numpy.array([0.2,0.01,0.1])
+    y = numpy.random.normal(numpy.dot(A,xin),dy)
+    print A,y,dy
+    s = wsolve.wsolve(A,y,dy)
+    print "xin,x,dx", xin, s.x, s.std
 
 Note there is a counter-intuitive result that scaling the estimated
 uncertainty in the data does not affect the computed uncertainty in
@@ -54,24 +58,62 @@ serves only to provide relative weighting between the points.
 
 import numpy as N
 
-class Confidence(object):
+# Grab erfc from scipy if it is available; if not then we can only
+# calculate confidence intervals for sigma = 1
+try: 
+    from scipy import stats
+    from scipy.special import erfc
+except:
+    # If scipy is missing we will not be able to calculate confidence
+    # intervals or prediction intervals.
+    pass
+
+class LinearModel(object):
     """
-    Confidence object returned from linear solver.
+    Model evaluator for linear solution to Ax = y.
 
-    Stored properties:
-    DoF = len(y)-len(x) = degrees of freedom
-    rnorm = 2-norm of the residuals y-Ax
-    x = solution to the equation Ax = y
+    Computes a confidence interval (range of likely values for the
+    mean at x) or a prediction interval (range of likely values 
+    seen when measuring at x).  The prediction interval tells
+    you the width of the distribution at x.  This should be the same
+    regardless of the number of measurements you have for the value
+    at x.  The confidence interval tells you how well you know the
+    mean at x.  It should get smaller as you increase the number of
+    measurements.  Error bars in the physical sciences usually show 
+    a 1-alpha confidence value of erfc(1/sqrt(2)), representing
+    a 1 sigma standandard deviation of uncertainty in the mean.
 
-    Computed properties:
-    cov = covariance matrix [ inv(A'A); O(n^3) ]
-    var = parameter variance [ diag(cov); O(n^2)]
-    std = standard deviation of parameters [ sqrt(var); O(n^2) ]
-    p = test statistic for chisquare goodness of fit [ chi2.sf; O(1) ]
+    Confidence intervals for linear system are given by::
 
-    Methods:
-    ci(A,sigma=1):  return confidence interval evaluated at A
-    pi(A,alpha=0.05):  return prediction interval evaluated at A
+        x' p +/- sqrt( Finv(1-a,1,df) var(x' p) )
+
+    where for confidence intervals::
+
+        var(x' p) = sigma^2 (x' inv(A'A) x)
+        
+    and for prediction intervals::
+    
+        var(x' p) = sigma^2 (1 + x' inv(A'A) x)
+
+
+    Stored properties::
+
+        DoF = len(y)-len(x) = degrees of freedom
+        rnorm = 2-norm of the residuals y-Ax
+        x = solution to the equation Ax = y
+
+    Computed properties::
+    
+        cov = covariance matrix [ inv(A'A); O(n^3) ]
+        var = parameter variance [ diag(cov); O(n^2)]
+        std = standard deviation of parameters [ sqrt(var); O(n^2) ]
+        p = test statistic for chisquare goodness of fit [ chi2.sf; O(1) ]
+
+    Methods::
+
+        ci(A,sigma=1):  return confidence interval evaluated at A
+        pi(A,alpha=0.05):  return prediction interval evaluated at A
+
     """
     def __init__(self, x=None, DoF=None, SVinv=None, rnorm=None):
         """
@@ -106,15 +148,48 @@ class Confidence(object):
     std = property(_std,doc="result standard deviation")
     p = property(_p,doc="probability of rejection")
 
-    def _interval(self,A,alpha,pred):
+    def _interval(self,X,alpha,pred):
         """
         Helper for computing prediction/confidence intervals.
         """
-        from scipy import stats
-        y = N.dot(A,self.x)
-        s = stats.t.ppf(1-alpha/2,self.DoF)*S.normr/sqrt(self.DoF)
-        dy = s*sqrt(pred+N.sum( (N.dot(self.invR,A))**2, axis=0))
+
+        # Comments from QR decomposition solution to Ax = y:
+        #
+        #   Rather than A'A we have R from the QR decomposition of A, but
+        #   R'R equals A'A.  Note that R is not upper triangular since we
+        #   have already multiplied it by the permutation matrix, but it
+        #   is invertible.  Rather than forming the product R'R which is
+        #   ill-conditioned, we can rewrite x' inv(A'A) x as the equivalent
+        #      x' inv(R) inv(R') x = t t', for t = x' inv(R)
+        #
+        # We have since switched to an SVD solver, which gives us
+        #
+        #    invC = A'A  = (USV')'USV' = VSU'USV' = VSSV'
+        #    C = inv(A'A) = inv(VSSV') = inv(V')inv(SS)inv(V)
+        #      = Vinv(SS)V' = Vinv(S) inv(S)V'
+        #
+        # Substituting, we get
+        #
+        #    x' inv(A'A) x = t t', for t = x' Vinv(S)
+        #
+        # Since x is a vector, t t' is the inner product sum(t**2).
+        # Note that LAPACK allows us to do this simultaneously for many
+        # different x using sqrt(sum(T**2,axis=1)), with T = X' Vinv(S). 
+        #
+        # Note: sqrt(F(1-a;1,df)) = T(1-a/2;df)
+        #
+        y = N.dot(X,self.x).ravel()
+        s = stats.t.ppf(1-alpha/2,self.DoF)*self.rnorm/N.sqrt(self.DoF)
+        t = N.dot(X,self._SVinv)
+        dy = s*N.sqrt(pred + N.sum( t**2, axis=1))
         return y,dy
+    
+    def __call__(self, A):
+        """
+        Return the prediction for a linear system at points in the
+        rows of A.
+        """
+        return N.dot(N.asarray(A),self.x)
 
     def ci(self, A, sigma=1):
         """
@@ -122,10 +197,12 @@ class Confidence(object):
         for the linear model evaluated at A.
 
         sigma=1 corresponds to a 1-sigma confidence interval
+
+        Confidence intervals are sometimes expressed as 1-alpha values,
+        where alpha = erfc(sigma/sqrt(2)).
         """
-        print "wsolve.Confidence.ci is not yet tested"
-        alpha = erfc(sigma/sqrt(2))
-        return _interval(A,alpha,0)
+        alpha = erfc(sigma/N.sqrt(2))
+        return self._interval(N.asarray(A),alpha,0)
 
     def pi(self, A, p=0.05):
         """
@@ -134,8 +211,7 @@ class Confidence(object):
 
         p = 1-alpha = 0.05 corresponds to 95% prediction interval
         """
-        print "wsolve.Confidence.pi is not yet tested"
-        return _interval(A,p,1)
+        return self._interval(N.asarray(A),p,1)
 
 def wsolve(A,y,dy=1,rcond=1e-12):
     """
@@ -151,7 +227,7 @@ def wsolve(A,y,dy=1,rcond=1e-12):
     # The ugliness vh.T.conj() is the hermitian transpose
 
     # Make sure inputs are arrays
-    A,y,dy = N.array(A),N.array(y),N.array(dy)
+    A,y,dy = N.asarray(A),N.asarray(y),N.asarray(dy)
     result_dims = y.ndim
     if dy.ndim == 1: dy = dy[:,N.newaxis]
     if y.ndim == 1: y = y[:,N.newaxis]
@@ -185,27 +261,110 @@ def wsolve(A,y,dy=1,rcond=1e-12):
     DoF = y.shape[0] - x.shape[0]
     rnorm = N.linalg.norm(y - N.dot(A,x))
 
-    return Confidence(x=x, DoF=DoF, SVinv=SVinv, rnorm=rnorm)
+    return LinearModel(x=x, DoF=DoF, SVinv=SVinv, rnorm=rnorm)
 
-def wpolyfit(x,y,dy=1,deg=None,origin=False):
+def _poly_matrix(x,degree,origin=False):
     """
-    Return the coefficients of a polynomial p(x) of degree n that
+    Generate the matrix A used to fit a polynomial using a linear solver.
+    """
+    if origin:
+        n = N.array(range(degree,0,-1))
+    else:
+        n = N.array(range(degree,-1,-1))
+    return N.asarray(x)[:,None]**n[None,:]
+
+class PolynomialModel(object):
+    """
+    Model evaluator for best fit polynomial p(x) = y.
+
+    Stored properties::
+
+        DoF = len(y)-len(x) = degrees of freedom
+        rnorm = 2-norm of the residuals y-Ax
+        coeff = coefficients
+        degree = polynomial degree
+        
+    Computed properties::
+    
+        cov = covariance matrix [ inv(A'A); O(n^3) ]
+        var = coefficient variance [ diag(cov); O(n^2)]
+        std = standard deviation of coefficients [ sqrt(var); O(n^2) ]
+        p = test statistic for chisquare goodness of fit [ chi2.sf; O(1) ]
+
+    Methods::
+
+        __call__(x): return the polynomial evaluated at x
+        ci(x,sigma=1):  return confidence interval evaluated at x
+        pi(x,alpha=0.05):  return prediction interval evaluated at x
+
+    Note that the covariance matrix will not include the ones column if
+    the polynomial goes through the origin.
+    """
+    def __init__(self, s, origin=False):
+        self.origin = origin
+        self.coeff = N.ravel(s.x)
+        if origin: self.coeff = N.hstack((self.coeff,0))
+        self.degree = len(self.coeff)-1
+        self.DoF = s.DoF
+        self.rnorm = s.rnorm
+        self._conf = s
+    def _cov(self):
+        return self._conf.cov
+    def _std(self):
+        return N.sqrt(self._var())
+    def _var(self):
+        var = N.ravel(self._conf.var)
+        if self.origin: var = N.hstack((var,0))
+        return var
+    def _p(self):
+        return self._conf.p
+    cov = property(_cov,doc="covariance matrix")
+    var = property(_var,doc="result variance")
+    std = property(_std,doc="result standard deviation")
+    p = property(_p,doc="probability of rejection")
+
+        
+    def __call__(self, x):
+        """
+        Evaluate the polynomial at x.
+        """
+        return N.polyval(x)
+
+    def ci(self, x, sigma=1):
+        """
+        Evaluate the polynomial and the confidence intervals at x.
+
+        sigma=1 corresponds to a 1-sigma confidence interval
+        """
+        A = _poly_matrix(x,self.degree,self.origin)
+        return self._conf.ci(A,sigma)
+
+    def pi(self, x, p=0.05):
+        """
+        Evaluate the polynomial and the prediction intervals at x.
+
+        p = 1-alpha = 0.05 corresponds to 95% prediction interval
+        """
+        A = _poly_matrix(x,self.degree,self.origin)
+        return self._conf.pi(A,p)
+
+    def __str__(self):
+        # TODO: better polynomial pretty printing using formatnum
+        return "Polynomial(%s)"%self.coeff
+    
+def wpolyfit(x,y,dy=1,degree=None,origin=False):
+    """
+    Return the polynomial of degree n that
     minimizes sum( (p(x_i) - y_i)**2/dy_i**2).
 
     if origin is True, the fit should go through the origin.
     """
-    assert deg != None, "Missing degree argument to wpolyfit"
-    if origin:
-        A = N.vstack(x**k for k in range(deg,0,-1))
-    else:
-        A = N.vstack(x**k for k in range(deg,-1,-1))
+    assert degree != None, "Missing degree argument to wpolyfit"
 
-    s = wsolve(A.T,y,dy)
-    p,dp = N.ravel(s.x),N.ravel(s.std)
-    if origin:
-        p = N.hstack((p,0))
-        dp = N.hstack((dp,0))
-    return p,dp
+    A = _poly_matrix(x,degree,origin)
+    s = wsolve(A,y,dy)
+    return PolynomialModel(s,origin=origin)
+
 
 def demo():
     import pylab
@@ -213,17 +372,21 @@ def demo():
     # Make fake data
     x = N.linspace(-15,5,15)
     th = N.polyval([.2,3,1,5],x)  # polynomial
-    dy = N.sqrt(N.abs(th))       # poisson uncertainty estimate
-    y = N.random.normal(th,dy)   # but normal generator
+    dy = N.sqrt(N.abs(th))        # poisson uncertainty estimate
+    y = N.random.normal(th,dy)    # but normal generator
 
     # Fit to a polynomial
-    p,dp = wpolyfit(x,y,dy=dy,deg=3)
+    poly = wpolyfit(x,y,dy=dy,degree=3)
 
     # Plot the result
     pylab.errorbar(x,y,yerr=dy,fmt='x')
     pylab.hold(True)
     px=N.linspace(x[0],x[-1],200)
-    pylab.plot(px,N.polyval(p,px))
+    py,pdy = poly.pi(px)
+    cy,cdy = poly.ci(px)
+    pylab.plot(px,py,'g-',
+               px,py+pdy,'g-.',px,py-pdy,'g-.',
+               px,cy+cdy,'r-.',px,cy-cdy,'r-.')
     pylab.show()
 
 def test():
@@ -234,17 +397,27 @@ def test():
     x = N.array([0,1,2,3,4],'d')
     y = N.array([  2.5,   7.9,  13.9,  21.1,  44.4],'d')
     dy = N.array([ 1.7,  2.4,  3.6,  4.8,  6.2],'d')
-    p,dp = wpolyfit(x,y,dy,1)
+    poly = wpolyfit(x,y,dy,1)
+    px = N.array([1.5],'d')
+    py,pi = poly.pi(px)
+    py,ci = poly.ci(px)
 
+    ## Uncomment these to show target values
     #print "Tp = [%.16g, %.16g]"%(p[0],p[1])
     #print "Tdp = [%.16g, %.16g]"%(dp[0],dp[1])
+    #print "Tpi,Tci = %.16g, %.16g"%(pi,ci)
     Tp = N.array([7.787249069840737, 1.503992847461524])
     Tdp = N.array([1.522338103010216, 2.117633626902384])
+    Tpi,Tci = 7.611128464981324, 2.342860389884832
 
-    perr = N.max(N.abs(p-Tp))
-    dperr = N.max(N.abs(dp-Tdp))
-    assert  perr < 1e-15,"||p-Tp||=%g"%perr
-    assert  dperr < 1e-15,"||dp-Tdp||=%g"%dperr
+    perr = N.max(N.abs(poly.coeff-Tp))
+    dperr = N.max(N.abs(poly.std-Tdp))
+    cierr = N.abs(ci-Tci)
+    pierr = N.abs(pi-Tpi)
+    assert perr < 1e-15,"||p-Tp||=%g"%perr
+    assert dperr < 1e-15,"||dp-Tdp||=%g"%dperr
+    assert cierr < 1e-15,"||ci-Tci||=%g"%cierr
+    assert pierr < 1e-15,"||pi-Tpi||=%g"%pierr
 
 if __name__ == "__main__":
     test()
