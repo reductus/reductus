4265fe7205c4ddc888f3e3c5465dcf0249fc14e8
pkienzle <pkienzle@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2008-11-18 15:43:47 +0000

    reduction: fix icp format load error
    git-svn-id: svn://danse.us/reflectometry/trunk/reflectometry/reduction@836 a92bf6fb-5b16-0410-9b54-b8becc4ab72c

--- lib/str2imat.c
+++ lib/str2imat.c
@@ -1,28 +1,18 @@
-/* public domain
-
-jazz:
-  LIBZ="-L/data/people/pkienzle/packages/zlib-1.4.4 -lz"
-  cc -O2 reflbin.c -o ~/bin/reflbin -lgen -lm $LIBZ
-
-linux, macosx:
-  gcc -Wall -O2 reflbin.c -o ~/bin/reflbin -lm -lz
-
-MinGW:
-  LIBZ="-L/usr/local/lib -lz"
-  gcc -Wall -O2 -I/usr/local/include reflbin.c -o reflbin.exe -lm $LIBZ
-
-Compile with -DDEBUG to show input and output.
-*/
+/* This program is public domain */
 
 #include <ctype.h>
 #include <stdio.h>
 
 void str2imat(const char str[], int size, int imat[], int *rows, int *columns)
 /*
- * Converts a string of the form ###, ###,###,###;###,###,###
+ * Converts a string of the form ###, ###,#,####;###,###,###,##
  * to an array of integers, returning the number of rows and columns.
  * There may be any amount of whitespace within and between the digits
  * of the number.
+ *
+ * Note that only the first 'size' numbers are preserved, so size should be the
+ * number of values in imat, or 0 if you just want to count the number of rows/
+ * columns in the block.
  */
 {
   int i = 0;      /* Current character position */
@@ -37,19 +27,20 @@ void str2imat(const char str[], int size, int imat[], int *rows, int *columns)
     if (isdigit(c)) {
       number = number*10 + c - '0';
     } else if (c==',') {
-      if (k == size) return;
-      imat[k++] = number;
+      if (k < size) imat[k++] = number;
       number = 0;
       nc++;
     } else if (c == ';') {
-      if (k == size) return;
-      imat[k++] = number;
+      if (k < size) imat[k++] = number;
       number = 0;
       nc = 0;
       nr++;
     }
     i++;
   }
+  /* The sequence should have ended with a number, so save it */
+  if (k < size) imat[k++] = number;
   *columns = ++nc;
   *rows = ++nr;
 }
+
