15dc6c005b3a6e46f698156a03a58d4fd50a33be
Brian Maranville <brian.maranville@nist.gov>
2015-02-02 21:49:48 -0500

    lots o stuff

---
+++ reflweb/js/plot_d3_rz.js
@@ -0,0 +1,326 @@
+function plotD3(target_id, data, log_x, log_y, show_line, show_points) {
+
+    //************************************************************
+    // Data notice the structure
+    //************************************************************
+    var default_data = 	[
+	    [{'x':1,'y':0},{'x':2,'y':5},{'x':3,'y':10},{'x':4,'y':0},{'x':5,'y':6},{'x':6,'y':11},{'x':7,'y':9},{'x':8,'y':4},{'x':9,'y':11},{'x':10,'y':2}],
+	    [{'x':1,'y':1},{'x':2,'y':6},{'x':3,'y':11},{'x':4,'y':1},{'x':5,'y':7},{'x':6,'y':12},{'x':7,'y':8},{'x':8,'y':3},{'x':9,'y':13},{'x':10,'y':3}],
+	    [{'x':1,'y':2},{'x':2,'y':7},{'x':3,'y':12},{'x':4,'y':2},{'x':5,'y':8},{'x':6,'y':13},{'x':7,'y':7},{'x':8,'y':2},{'x':9,'y':4},{'x':10,'y':7}],
+	    [{'x':1,'y':3},{'x':2,'y':8},{'x':3,'y':13},{'x':4,'y':3},{'x':5,'y':9},{'x':6,'y':14},{'x':7,'y':6},{'x':8,'y':1},{'x':9,'y':7},{'x':10,'y':9}],
+	    [{'x':1,'y':4},{'x':2,'y':9},{'x':3,'y':14},{'x':4,'y':4},{'x':5,'y':10},{'x':6,'y':15},{'x':7,'y':5},{'x':8,'y':0},{'x':9,'y':8},{'x':10,'y':5}]
+    ];
+    
+    var data = (data == null)? default_data : data;
+    
+    console.log(data, log_x, log_y);
+    
+    
+    var max_y = -Infinity;
+    var min_y = Infinity;
+    var max_x = -Infinity;
+    var min_x = Infinity;
+    
+    var linf = function(x) { return x }
+    var logf = function(x) { return Math.log(x) / Math.LN10 }
+    
+    var tx = log_x ? logf : linf;
+    var ty = log_y ? logf : linf;
+    
+    var newy, newx;
+    var labels = data.options.series.map(function(d) { return d.label });
+    var transformed_data = [];
+    
+    for (var i=0; i<data.data.length; i++) {
+        var newSet = data.data[i].map(
+            function(p) {
+                newx = tx(p[0]);
+                newy = ty(p[1]);
+                if (isFinite(newx)) {
+                    if (newx > max_x) max_x = tx(p[0]);
+                    if (newx < min_x) min_x = tx(p[0]);
+                }
+                if (isFinite(newy)) {
+                    if (newy > max_y) max_y = ty(p[1]);
+                    if (newy < min_y) min_y = ty(p[1]); 
+                }
+                return {'x': newx, 'y': newy} 
+            }
+        );
+        transformed_data.push(newSet);
+    }
+    
+    data = transformed_data;
+    console.log(transformed_data);
+    
+    var old_colors = [
+	    'steelblue',
+	    'green',
+	    'red',
+	    'purple'
+    ]
+
+    var colors = [
+        "#4bb2c5", 
+        "#EAA228", 
+        "#c5b47f", 
+        "#579575", 
+        "#839557", 
+        "#958c12", 
+        "#953579", 
+        "#4b5de4", 
+        "#d8b83f", 
+        "#ff5800", 
+        "#0085cc", 
+        "#c747a3", 
+        "#cddf54", 
+        "#FBD178", 
+        "#26B4E3", 
+        "#bd70c7"
+    ] 
+     
+    //************************************************************
+    // Create Margins and Axis and hook our zoom function
+    //************************************************************
+    
+    var container = d3.select(target_id).node();
+    
+    var margin = {top: 20, right: 30, bottom: 50, left: 50},
+        width = container.clientWidth - margin.left - margin.right - 25,
+        height = container.clientHeight - margin.top - margin.bottom - 25;
+	
+	var x = d3.scale.linear()
+        .domain([min_x, max_x])
+        .range([0, width]);
+     
+    var y = d3.scale.linear()
+        .domain([min_y, max_y])
+        .range([height, 0]);
+	
+    var xAxis = d3.svg.axis()
+        .scale(x)
+	    .tickSize(-height)
+	    .ticks(5)
+	    .tickPadding(10)	
+	    .tickSubdivide(true)	
+        .orient("bottom");	
+	
+    var yAxis = d3.svg.axis()
+        .scale(y)
+	    .tickPadding(10)
+	    .ticks(5)
+	    .tickSize(-width)
+	    .tickSubdivide(true)	
+        .orient("left");
+    
+    var zoom = d3.behavior.zoom().x(x).y(y).on("zoom", zoomed);
+
+    var zoomRect = true;
+    
+    d3.select("#zoom-rect").on("change", function() {
+      zoomRect = this.checked;
+    });
+    
+    //************************************************************
+    // Generate our SVG object
+    //************************************************************	
+    var svg = d3.select(target_id).append("svg")
+	    .call(zoom)
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        //.attr("width", "100%")
+        //.attr("height", "100%")
+	    .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .call(zoom)
+      .append("g")
+        .on("mousedown", function() {
+          if (!zoomRect) return;
+          var e = this,
+              origin = d3.mouse(e),
+              rect = svg.append("rect").attr("class", "zoom");
+          d3.select("body").classed("noselect", true);
+          origin[0] = Math.max(0, Math.min(width, origin[0]));
+          origin[1] = Math.max(0, Math.min(height, origin[1]));
+          d3.select(window)
+              .on("mousemove.zoomRect", function() {
+                var m = d3.mouse(e);
+                m[0] = Math.max(0, Math.min(width, m[0]));
+                m[1] = Math.max(0, Math.min(height, m[1]));
+                rect.attr("x", Math.min(origin[0], m[0]))
+                    .attr("y", Math.min(origin[1], m[1]))
+                    .attr("width", Math.abs(m[0] - origin[0]))
+                    .attr("height", Math.abs(m[1] - origin[1]));
+              })
+              .on("mouseup.zoomRect", function() {
+                d3.select(window).on("mousemove.zoomRect", null).on("mouseup.zoomRect", null);
+                d3.select("body").classed("noselect", false);
+                var m = d3.mouse(e);
+                m[0] = Math.max(0, Math.min(width, m[0]));
+                m[1] = Math.max(0, Math.min(height, m[1]));
+                console.log(m, origin);
+                if (m[0] !== origin[0] && m[1] !== origin[1]) {
+                  zoom.x(x.domain([origin[0], m[0]].map(x.invert).sort()))
+                      .y(y.domain([origin[1], m[1]].map(y.invert).sort()));
+                } else {
+                    console.log('else');
+                    zoom.scale(1);
+                    zoom.translate([0,0]);
+                }
+                rect.remove();
+                zoomed();
+              }, true);
+          d3.event.stopPropagation();
+        });
+    
+    svg.append("rect")
+        .attr("width", width)
+        .attr("height", height);
+            
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+	    .attr("class", "x axis")
+	    .append("text")
+	    .attr("class", "axis-label")
+	    .attr("x", width/2.0)
+	    //.attr("transform", "translate(0," + height + ")")
+        .attr("y", height + 40)
+	    .text('x-axis');
+     
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+     
+    svg.append("g")
+	    .attr("class", "y axis")
+	    .append("text")
+	    .attr("class", "axis-label")
+	    .attr("transform", "rotate(-90)")
+	    .attr("y", (-margin.left) + 10)
+	    .attr("x", -height/2)
+	    .text('Intensity (arb. units)');	
+     
+    svg.append("clipPath")
+	    .attr("id", "clip")
+	    .append("rect")
+	    .attr("width", width)
+	    .attr("height", height);
+	
+    //************************************************************
+    // Create D3 legend
+    //************************************************************	
+	var legend = svg.append("g")
+	  .attr("class", "legend")
+	  .attr("x", width - 65)
+	  .attr("y", 25)
+	  .attr("height", 100)
+	  .attr("width", 100);
+	
+	legend.selectAll('g').data(data)
+      .enter()
+      .append('g')
+      .each(function(d, i) {
+        var g = d3.select(this);
+        g.append("rect")
+          .attr("x", width - 65)
+          .attr("y", i*25)
+          .attr("width", 10)
+          .attr("height", 10)
+          .style("fill", colors[i%colors.length]);
+        
+        g.append("text")
+          .attr("x", width - 50)
+          .attr("y", i * 25 + 8)
+          .attr("height",30)
+          .attr("width",100)
+          .style("fill", colors[i])
+          .text(labels[i]);
+
+      });
+	
+    //************************************************************
+    // Create D3 line object and draw data on our SVG object
+    //************************************************************
+    if (show_line) {
+        var line = d3.svg.line()
+            .defined(function(d) { return isFinite(d.y); })
+            .interpolate("linear")	
+            .x(function(d) { return x(d.x); })
+            .y(function(d) { return y(d.y); });		
+
+        svg.selectAll('.line')
+            .data(data)
+            .enter()
+            .append("path")
+            .attr("class", "line")
+            .attr("clip-path", "url(#clip)")
+            .attr('stroke', function(d,i){ 			
+	            return colors[i%colors.length];
+            })
+            .attr("d", line);
+    } else {
+        var line = null;
+    }
+	
+	
+	
+	
+    //************************************************************
+    // Draw points on SVG object based on the data given
+    //************************************************************
+    if (show_points) {
+        var points = svg.selectAll('.dots')
+	        .data(data)
+	        .enter()
+	        .append("g")
+            .attr("class", "dots")
+	        .attr("clip-path", "url(#clip)");	
+         
+        points.selectAll('.dot')
+	        .data(function(d, index){ 		
+		        var a = [];
+		        d.forEach(function(point,i){
+			        a.push({'index': index, 'point': point});
+		        });		
+		        return a;
+	        })
+	        .enter()
+	        .append('circle')
+	        .attr('class','dot')
+	        .attr("r", 2.5)
+	        .attr('fill', function(d,i){ 	
+		        return colors[d.index%colors.length];
+	        })	
+	        .attr("transform", function(d) { 
+		        return "translate(" + x(d.point.x) + "," + y(d.point.y) + ")"; }
+	        );
+    } else {
+        var points = null;
+    }	
+     
+	
+	
+	
+	
+    //************************************************************
+    // Zoom specific updates
+    //************************************************************
+    function zoomed() {
+	    svg.select(".x.axis").call(xAxis);
+	    svg.select(".y.axis").call(yAxis);   
+	    if (line) svg.selectAll('path.line').attr('d', line);  
+     
+	    if (points) points.selectAll('circle').attr("transform", function(d) { 
+		    return "translate(" + x(d.point.x) + "," + y(d.point.y) + ")"; }
+	    );  
+    }
+    
+    function refresh() {
+      svg.select(".x.axis").call(xAxis);
+      svg.select(".y.axis").call(yAxis);
+    }
+}
---
+++ reflweb/server_one_d3.py
@@ -0,0 +1,249 @@
+import sys, os
+import SocketServer
+import SimpleHTTPServer
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer, SimpleJSONRPCRequestHandler, SimpleJSONRPCDispatcher
+import socket
+
+import urlparse
+
+from OpenSSL import SSL
+
+import jsonrpclib.config
+
+import multiprocessing
+import webbrowser
+
+jsonrpclib.config.use_jsonclass = False
+
+"""
+HandlerClass = SimpleHTTPRequestHandler
+ServerClass  = BaseHTTPServer.HTTPServer
+Protocol     = "HTTP/1.0"
+
+if sys.argv[1:]:
+    port = int(sys.argv[1])
+else:
+    port = 8000
+server_address = ('localhost', 8001) # get next open socket
+
+HandlerClass.protocol_version = Protocol
+httpd = ServerClass(server_address, HandlerClass)
+http_port = httpd.socket.getsockname()[1]
+print "http port: ", http_port
+
+sa = httpd.socket.getsockname()
+print "Serving HTTP on", sa[0], "port", sa[1], "..."
+# httpd.serve_forever()
+#http_process = multiprocessing.Process(target=httpd.serve_forever)
+#http_process.start()
+"""
+
+currdir = os.path.dirname( __file__ )
+
+class JSONRPCRequestHandler(SimpleHTTPRequestHandler, SimpleJSONRPCRequestHandler):
+    """JSON-RPC and documentation request handler class.
+
+    Handles all HTTP POST requests and attempts to decode them as
+    XML-RPC requests.
+
+    Handles all HTTP GET requests and interprets them as requests
+    for web pages, js, json or css.
+    
+    Put all static files to be served in 'static' subdirectory.
+    """
+    #rpc_paths = ('/', '/RPC2')
+    rpc_paths = () # accept all
+    
+    def do_OPTIONS(self):
+        self.send_response(200, "ok")       
+        #self.send_header('Access-Control-Allow-Origin', 'http://localhost:8000')
+        #self.send_header('Access-Control-Allow-Origin', "http://localhost:%d" % (http_port,))           
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.end_headers()
+        self.connection.shutdown(0)
+        
+    # Add these headers to all responses
+    def end_headers(self):
+        self.send_header("Access-Control-Allow-Headers", 
+                         "Origin, X-Requested-With, Content-Type, Accept")
+        self.send_header("Access-Control-Allow-Origin", "*") #"http://localhost:%d" % (server_address[1],))
+        SimpleJSONRPCRequestHandler.end_headers(self)
+        
+    
+class SecureJSONRPCServer(BaseHTTPServer.HTTPServer, SimpleJSONRPCDispatcher):
+    #request_queue_size = 256
+    def __init__(self, server_address, HandlerClass, logRequests=True):
+        """Secure XML-RPC server.
+
+        It it very similar to SimpleXMLRPCServer but it uses HTTPS for transporting XML data.
+        """
+        self.logRequests = logRequests
+
+        SimpleJSONRPCDispatcher.__init__(self)
+        SocketServer.BaseServer.__init__(self, server_address, HandlerClass)
+        self.socket = socket.socket(self.address_family,
+                                    self.socket_type)
+        #ctx = SSL.Context(SSL.SSLv23_METHOD)
+        #ctx.use_privatekey_file (self.KEYFILE)
+        #ctx.use_certificate_file(self.CERTFILE)
+        #self.socket = SSL.Connection(ctx, socket.socket(self.address_family,
+        #                                                self.socket_type))
+        self.server_bind()
+        self.server_activate()
+        
+server = SimpleJSONRPCServer(('localhost', 0), encoding='utf8', requestHandler=JSONRPCRequestHandler)
+#server = SecureJSONRPCServer(('localhost', 0), JSONRPCRequestHandler)
+rpc_port = server.socket.getsockname()[1]
+webbrowser.open_new_tab('http://localhost:%d/index_one_d3.html' % (rpc_port,))
+server.register_function(pow)
+server.register_function(lambda x,y: x+y, 'add')
+server.register_function(lambda x: x, 'ping')
+
+import h5py, os, simplejson
+
+def categorize_files(path='./'):
+    fns = os.listdir(path)
+    fns.sort()
+    categories = {\
+        'Specular': 'SPEC',
+        'Background': 'BG', 
+        'Rocking': 'ROCK',
+        'Slit': 'SLIT'}
+    output = {}
+    for fn in fns:
+        try:
+            f = h5py.File(os.path.join(path, fn))
+            for entry in f.keys():
+                _name = f[entry].get('DAS_logs/sample/name').value.flatten()[0]
+                output.setdefault(_name, {})
+                _num = f[entry].get('DAS_logs/trajectoryData/fileNum').value.flatten()[0]
+                _scanType = f[entry].get('DAS_logs/trajectoryData/_scanType')
+                if _scanType is not None:
+                    _scanType = _scanType.value.flatten()[0]
+                elif f[entry].get('title', None).value.flatten()[0] == 'fp':
+                    _scanType = 'findPeak'
+                else:
+                    _scanType = 'uncategorized'
+                output[_name].setdefault(_scanType, {})
+                output[_name][_scanType]['%d:%s' % (_num, entry)] = {'filename': fn, 'entry': entry}
+        except:
+            pass
+            
+    #return simplejson.dumps(output)
+    return output
+
+def get_file_metadata(path='./'):
+    fns = os.listdir(path)
+    fns.sort()
+    metadata = []
+    for fn in fns:
+        try:
+            f = h5py.File(os.path.join(path, fn))
+            for entry in f.keys():
+                output = {}
+                _name = f[entry].get('DAS_logs/sample/name').value.flatten()[0]
+                output['sample_name'] = str(_name)
+                _num = f[entry].get('DAS_logs/trajectoryData/fileNum').value.flatten()[0]
+                output['fileNum'] = "%d" % (_num,)
+                _scanType = f[entry].get('DAS_logs/trajectoryData/_scanType')
+                if _scanType is not None:
+                    _scanType = _scanType.value.flatten()[0]
+                elif f[entry].get('title', None).value.flatten()[0] == 'fp':
+                    _scanType = 'findPeak'
+                else:
+                    _scanType = 'uncategorized'
+                output['scanType'] = _scanType
+                output['filename'] = fn
+                output['path'] = path
+                output['entry'] = entry
+                metadata.append(output)
+        except:
+            pass
+    return metadata
+
+def get_jstree(path='./'):
+    files = categorize_files(path)
+    categories = ['SPEC','BG','ROCK','SLIT','findPeak','uncategorized']
+    output = {'core': {'data': []}}
+    sample_names = files.keys()
+    for sample in sample_names:
+        samp_out = {"text": sample}
+        samp_out['children'] = []
+        for cat in categories:
+            if not cat in files[sample]: break
+            cat_out = {"text": cat, "children": []}
+            item_keys = files[sample][cat].keys()
+            item_keys.sort()
+            for child in item_keys:
+                cat_out['children'].append({"text": child, "extra_data": {
+                    "filename": files[sample][cat][child]['filename'],
+                    "entry": files[sample][cat][child]['entry'],
+                    "path": path}});
+            samp_out['children'].append(cat_out)
+        output['core']['data'].append(samp_out)
+    return output 
+        
+def get_plottable(file_and_entry):
+    """ file_and_entry should be list of dicts:
+    [{ "filename": fn1, "entry": entryname1 }, { "filename": fn2, ...} ...]
+    """
+    print file_and_entry
+    fig = {
+        "title": "plot",
+        "type": "1d",
+        "data": [],
+        "options": {
+            "axes": {"xaxis": {}, "yaxis": {}},
+            "series": [],
+            },
+        }
+    for item in file_and_entry:
+        f = h5py.File(item['filename'])
+        DAS = f[item['entry']]['DAS_logs']
+        print DAS
+        xAxis = DAS.get('trajectoryData/xAxis')
+        if xAxis is not None:
+            xAxis = xAxis.value[0].replace('.', '/')
+        if xAxis in DAS.keys() and 'primary' in DAS[xAxis].attrs: 
+            # then it's a device name: convert to primary node
+            xAxis = xAxis + "/" + DAS[xAxis].attrs['primary']
+        if not xAxis in DAS:
+            xAxis = DAS.get('trajectory/defaultXAxisPlotNode', "").value[0].replace('.', '/')
+        if xAxis == "":
+            xAxis = DAS['trajectory/scannedVariables'].value[0].split()[0].replace('.', '/')
+        
+        yAxis=DAS.get('trajectory/defaultYAxisPlotNode', "").value[0].replace('.', '/')
+        if yAxis == "":
+            yAxis = "counter/liveROI"
+        yAxisChannel = DAS.get('trajectory/defaultYAxisPlotChannel', -1)
+        fig_x = fig['options']['axes']['xaxis']
+        fig_y = fig['options']['axes']['yaxis']
+        if "label" in fig_x:
+            if not fig_x['label'] == xAxis:
+                raise Exception("axes do not match")
+        else:
+            fig_x['label'] = xAxis
+        if "label" in fig_y:
+            if not fig_y['label'] == yAxis:
+                raise Exception("axes do not match")
+        else: 
+            fig_y['label'] = yAxis
+        fig['options']['series'].append({"label": item['filename'] + ':' + item['entry']})
+        x = DAS[xAxis].value.astype('float')
+        y = DAS[yAxis].value.astype('float')
+        xy = [[xx,yy] for xx, yy in zip(x,y)]
+        fig['data'].append(xy)
+        
+    fig['options']['axes']['xaxis']['label'] = fig['options']['axes']['xaxis']['label'].replace('/', '.')
+    fig['options']['axes']['yaxis']['label'] = fig['options']['axes']['yaxis']['label'].replace('/', '.')
+    return fig
+    
+server.register_function(categorize_files) # deprecated
+server.register_function(get_plottable)
+server.register_function(get_jstree) # deprecated
+server.register_function(get_file_metadata)
+server.serve_forever()
+print "done serving rpc forever"
+httpd_process.terminate()
