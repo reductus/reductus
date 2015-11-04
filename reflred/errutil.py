95c052e26cf7b2851d7f3876d7a07b3e65423ed3
Paul Kienzle <pkienzle@nist.gov>
2015-11-04 17:38:25 -0500

    modify interface for uncertainties package helper functions

--- reflred/errutil.py
+++ reflred/errutil.py
@@ -1,11 +1,24 @@
 """
 Extensions to the uncertainties package.
+
+:func:`interp` is a drop-in replacement for numpy.interp which operates on
+uncertainty values.
+
+:func:`interp_err` and :func:`solve_err` operate on value and uncertainty in
+different variables, but uses the machinery of the uncertainties package to
+do the logic.
+
+:func:`format_err` is a helper function for formatting value + uncertainty in
+different variables.
 """
-import uncertainties as err
-import uncertainties.unumpy as nperr
+
+
+from uncertainties import ufloat
+from uncertainties.unumpy import \
+    uarray, umatrix, nominal_values as uval, std_devs as udev
 import numpy as np
 
-def interp(x,xp,fp,dfp,left=None,right=None):
+def interp_err(x, xp, fp, dfp, left=None, right=None):
     """
     Linear interpolation of x into points (xk,yk +/- dyk).
 
@@ -17,37 +30,67 @@ def interp(x,xp,fp,dfp,left=None,right=None):
     right is the uncertainty value to return for points after the range of xp,
     or None for the final value, fp[-1].
     """
-    if (isinstance(x, (float, int, np.number))
-            or isinstance(x, np.ndarray) and x.ndim == 0):
-        fp = err.ufloat(fp,dfp)
-        f = _interp([x], xp, fp, left, right)[0]
-        return f.n, f.s**2
+    is_scalar_x = np.isscalar(x)
+    if is_scalar_x:
+        fp = ufloat(fp, dfp)
+        f = interp([x], xp, fp, left, right)[0]
+        return f.n, f.s
     else:
-        fp = nperr.uarray(fp,dfp)
-        f = _interp(x, xp, fp, left, right)
-        return np.array([xi.n for xi in f]),np.array([xi.s for xi in f])
+        fp = uarray(fp, dfp)
+        f = interp(x, xp, fp, left, right)
+        return uval(f), udev(f)
 
-def _interp(x,xp,fp,left=None,right=None):
-    idx = np.searchsorted(xp[1:-1], x)
-    # Support repeated values in Xp, which will lead to 0/0 errors if the
-    # interpolated point is one of the repeated values.
-    p = (xp[idx+1]-x)/(xp[idx+1]-xp[idx])
-    f = p*fp[idx] + (1-p)*fp[idx+1]
-    if left is None: left = fp[0]
-    if right is None: right = fp[-1]
-    f[x<xp[0]] = left if left is not None else fp[0]
-    f[x>xp[-1]] = right if right is not None else fp[-1]
-
-    return f
-
-def format(x,dx):
+def format_err(x, dx):
     """
     Format number with uncertainty.
     """
-    return err.ufloat(x,dx).format('gS')
+    return ufloat(x, dx).format('gS')
 
-def solve(A,dA,b,db):
-    A = nperr.umatrix(A,dA)
-    b = nperr.umatrix(b,db).T
+
+def solve_err(A, dA, b, db):
+    A = umatrix(A, dA)
+    b = umatrix(b, db).T
     x = A.I*b
-    return x.nominal_values, x.std_devs
+    return uval(x), udev(x)
+
+
+def interp(x, xp, fp, left=None, right=None):
+    """
+    1-D interpolation of *x* into *(xp,fp)*.
+
+    *xp* must be an increasing vector.  *x* can be scalar or vector.
+
+    If *x* is beyond the range of *xp*, returns *left/right*, or the value of
+    *fp* at the end points if *left/right* is not defined.
+
+    Implemented in pure python so *fp* can be an extended numeric type such
+    as complex or value+uncertainty.
+    """
+    is_scalar_x = np.isscalar(x)
+    if len(xp) == 1:
+        f = fp[np.zeros_like(x, dtype='i')]
+    else:
+        xp = np.asarray(xp)
+        if np.any(np.diff(xp)<=0.):
+            raise ValueError("interp needs a sorted list")
+        if not is_scalar_x:
+            x = np.asarray(x)
+        idx = np.searchsorted(xp[1:-1], x)
+        # Support repeated values in Xp, which will lead to 0/0 errors if the
+        # interpolated point is one of the repeated values.
+        p = (xp[idx+1]-x)/(xp[idx+1]-xp[idx])
+        f = p*fp[idx] + (1-p)*fp[idx+1]
+
+    if is_scalar_x:
+        if x < xp[0]:
+            return left if left is not None else fp[0]
+        elif x > xp[-1]:
+            return right if right is not None else fp[-1]
+        else:
+            return f
+    else:
+        f[x<xp[0]] = left if left is not None else fp[0]
+        f[x>xp[-1]] = right if right is not None else fp[-1]
+        return f
+
+
