09c23845a363f6d06a35fe97d4d628ca934ae2c4
Brian Maranville <brian.maranville@nist.gov>
2016-02-05 15:49:30 -0500

    trying to add more reduction modules

--- dataflow/modules/load.py
+++ dataflow/modules/load.py
@@ -94,12 +94,16 @@ load_kw = {
 load_module = Module(**load_kw)
 
 def mask_action(input=None, mask_indices=None, **kwargs):
-    import numpy
-    if input is not None: 
+    """ take sparse index lists for masking
+    e.g. mask_indices = {"0": [1,4,6], "5": [0]}
+    operates to put masks on data items 0 and 5
+    """
+    if hasattr(input, '__iter__') and hasattr(mask_indices, '__contains__') :
+        import numpy 
         for i, data in enumerate(input):
             data.mask = numpy.ones(data.detector.counts.shape, dtype="bool")
-            if mask_indices is not None:
-                for j in mask_indices[i]:
+            if str(i) in mask_indices:
+                for j in mask_indices[str(i)]:
                     data.mask[j] = False
     return dict(output=input)
     
@@ -136,6 +140,12 @@ mask_kw = {
 
 mask_module = Module(**mask_kw)
 
+def normalize_action(input=None, base='auto'):
+    from reflred.steps import scale
+    for data in input:
+        scale.apply_norm(data, base)
+    return dict(output=input)
+    
 normalize_kw = {
     "id": "ncnr.refl.normalize",
     "version": 0.1,
@@ -162,13 +172,49 @@ normalize_kw = {
             "value": "auto"
         }
     },
-    "action": steps.normalize,
-    "name": "Load NeXuS Reflectometry Data"
+    "action": normalize_action,
+    "name": "Normalize NeXuS Reflectometry Data"
     
 }
 
 normalize_module = Module(**normalize_kw)
 
+def join_action(input=None, tolerance=0.0):
+    from reflred.steps import joindata
+    return dict(output=joindata.join_datasets(input, tolerance))
+    
+join_kw = {
+    "id": "ncnr.refl.join",
+    "version": 0.1,
+    "description": "join reflectometry NeXus files",
+    "terminals": [
+        {
+            "id": "output", 
+            "datatype": "ncnr.refl.data",
+            "use": "out",
+            "description": "joined data"
+        },
+        {
+            "id": "input",
+            "datatype": "ncnr.refl.data",
+            "use": "in",
+            "description": "source data"
+        }        
+    ],
+    "fields": {
+        "tolerance": {
+            "type":"number",
+            "label": "tolerance",
+            "name": "tolerance",
+            "value": 0.0
+        }
+    },
+    "action": join_action,
+    "name": "join NeXuS Reflectometry Data"
+    
+}
+
+join_module = Module(**join_kw)
 
 
 
@@ -197,17 +243,22 @@ def test():
     rdata = Data("ncnr.refl.data", ReflData, loaders=[{'function':load_action, 'id':'LoadNeXuS'}])
     register_module(load_module)
     register_module(normalize_module)
+    register_module(join_module)
     register_module(mask_module)
     register_datatype(rdata)
     # a = load_action(files=test_dataset)
     #modules = [{"module": "ncnr.refl.load", "version": 0.1, "config": {"files": test_dataset}}]
     modules = [
         {"module": "ncnr.refl.load", "version": 0.1, "config": {}},
-        {"module": "ncnr.refl.mask", "version": 0.1, "config": {}}
+        {"module": "ncnr.refl.mask", "version": 0.1, "config": {}},
+        {"module": "ncnr.refl.join", "version": 0.1, "config": {}}
     ]
-    wires = [{"source": [0,"output"], "target": [1,"input"]}]
+    wires = [
+        {"source": [0,"output"], "target": [1,"input"]},
+        {"source": [1,"output"], "target": [2,"input"]},
+    ]
     template = Template("test", "test template", modules, wires, "ncnr.magik", version='0.0')
-    refl = calc_single(template, {"0": {"files": test_dataset}, "1": {"mask_indices": [[0,], [], []]}}, 1, "output")
+    refl = calc_single(template, {"0": {"files": test_dataset}, "1": {"mask_indices": {"0": [0,-1]}}}, 1, "output")
     return refl
     
 if __name__ == "__main__":
