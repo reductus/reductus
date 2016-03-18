af6fa6a61c69db45a132c6a993ec90ef244a4a86
Brian Maranville <brian.maranville@nist.gov>
2016-03-18 11:04:27 -0400

    TESTING: cache url_get values (files written directly to cache as binary strings)

--- dataflow/modules/load.py
+++ dataflow/modules/load.py
@@ -2,6 +2,10 @@ import urllib2
 import datetime
 import time
 from posixpath import basename, join
+from dataflow.calc import _format_ordered
+from dataflow.cache import get_cache
+import hashlib
+#
 
 import pytz
 
@@ -18,30 +22,42 @@ def check_datasource(source):
 
 def url_get(fileinfo):
     path, mtime, entries = fileinfo['path'], fileinfo['mtime'], fileinfo['entries']
-    source = fileinfo.get("source", DEFAULT_DATA_SOURCE)
-    name = basename(path)
-    check_datasource(source)
-    full_url = join(DATA_SOURCES[source], path)
-    url = None
-    print "loading", full_url
-    try:
-        url = urllib2.urlopen(full_url)
-        url_time_struct = url.info().getdate('last-modified')
-        t_repo = datetime.datetime(*url_time_struct[:7], tzinfo=pytz.utc)
-        t_request = datetime.datetime.fromtimestamp(mtime, pytz.utc)
-        if t_request > t_repo:
-            print "request mtime = %s, repo mtime = %s"%(t_request, t_repo)
-            raise ValueError("Requested mtime is newer than repository mtime for %r"%path)
-        elif t_request < t_repo:
-            print "request mtime = %s, repo mtime = %s"%(t_request, t_repo)
-            raise ValueError("Requested mtime is older than repository mtime for %r"%path)
+    # fingerprint the get, leaving off entries information:
+    cache = get_cache()
+    fileinfo_minimal = {'path': path, 'mtime': mtime}
+    config_str = str(_format_ordered(fileinfo_minimal))
+    parts = ["url_get", config_str]
+    fp = hashlib.sha1(":".join(parts)).hexdigest()
+    if cache.exists(fp):
+        ret = cache.get(fp)
+        print "getting " + path + " from cache!"
+    else:
+        source = fileinfo.get("source", DEFAULT_DATA_SOURCE)
+        name = basename(path)
+        check_datasource(source)
+        full_url = join(DATA_SOURCES[source], path)
+        url = None
+        print "loading", full_url
+        try:
+            url = urllib2.urlopen(full_url)
+            url_time_struct = url.info().getdate('last-modified')
+            t_repo = datetime.datetime(*url_time_struct[:7], tzinfo=pytz.utc)
+            t_request = datetime.datetime.fromtimestamp(mtime, pytz.utc)
+            if t_request > t_repo:
+                print "request mtime = %s, repo mtime = %s"%(t_request, t_repo)
+                raise ValueError("Requested mtime is newer than repository mtime for %r"%path)
+            elif t_request < t_repo:
+                print "request mtime = %s, repo mtime = %s"%(t_request, t_repo)
+                raise ValueError("Requested mtime is older than repository mtime for %r"%path)
 
-        ret = url.read()
-    except urllib2.HTTPError as exc:
-        raise ValueError("Could not open %r\n%s"%(path, str(exc)))
-    finally:
-        if url is not None:
-            url.close()
+            ret = url.read()
+            print "caching " + path
+            cache.set(fp, ret)
+        except urllib2.HTTPError as exc:
+            raise ValueError("Could not open %r\n%s"%(path, str(exc)))
+        finally:
+            if url is not None:
+                url.close()
         
     return ret
 
