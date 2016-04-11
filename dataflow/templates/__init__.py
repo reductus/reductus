008aaf326b16544eef1dd09fa52c023d12d21f5a
Brian Maranville <brian.maranville@nist.gov>
2016-04-10 21:27:57 -0400

    minor css change to make menus more pleasing

--- dataflow/templates/__init__.py
+++ dataflow/templates/__init__.py
@@ -3,6 +3,6 @@ def get_templates():
     import os, json
     template_path = os.path.dirname(__file__)
     template_names = [fn for fn in os.listdir(template_path) if fn.endswith(".json")]
-    templates = dict([(tn, json.loads(open(os.path.join(template_path, tn), 'r').read())) for tn in template_names])
+    templates = dict([(tn[:-5], json.loads(open(os.path.join(template_path, tn), 'r').read())) for tn in template_names])
     return templates
 
