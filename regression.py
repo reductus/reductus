d6bf28d23d80526411cdfddef947c64bf23afbb8
Paul Kienzle <pkienzle@nist.gov>
2016-12-18 15:38:42 -0500

    add a regression test tool which can check that reduction values haven't changed

---
+++ regression.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+"""
+Rerun the template from an exported data set and compare the output.
+
+Usage:
+
+    python regression.py output.dat
+
+Exits with 0 if there are no differences in output, or 1 if the output has
+changed.  The output is stored in a file in /tmp [sorry windows users], so
+that the regression test can be quickly updated if the change is a valid
+change (e.g., if there is a bug fix in the monitor normalization for example).
+
+The location of the data sources is read from reflweb.config.
+"""
+from __future__ import print_function
+
+import sys
+import os
+import json
+import re
+import importlib
+import difflib
+
+# Set the data sources
+from reflweb import config
+from dataflow.cache import set_test_cache
+from dataflow.core import Template
+from dataflow.calc import process_template
+from dataflow.modules import load
+
+# Match the following on the first line:
+# - initial comment ("#" or "//")
+# - the word template or template_data, perhaps in single or double quotes
+# - optional separator (":" or "=")
+# - open brace of template
+TEMPLATE = re.compile(r"^(#|//) *([\"']?template(_data)?[\"']?)? *[:=]? *\{")
+
+
+LOADED_INSTRUMENTS = set()
+def load_instrument(instrument_id):
+    # type: (str) -> None
+    """
+    Load the dataflow instrument definition given the instrument name.
+    """
+    if instrument_id not in LOADED_INSTRUMENTS:
+        instrument_module_name = 'dataflow.modules.'+instrument_id
+        instrument_module = importlib.import_module(instrument_module_name)
+        instrument_module.define_instrument()
+
+
+def show_diff(old, new, show_diff=True):
+    # type: (str, str) -> bool
+    """
+    Compare *old* and *new* text, returning True if they differ.
+
+    *show_diff* is True if the differences should be printed to stdout.
+
+    The text should be a multi-line string.
+    """
+    has_diff = False
+    delta = difflib.unified_diff(old.splitlines(True),
+                                 new.splitlines(True),
+                                 fromfile="old", tofile="new")
+    for line in delta:
+        if show_diff:
+            sys.stdout.write(line)
+        has_diff = True
+
+    return has_diff
+
+
+def replay_file(filename):
+    # type: (str) -> None
+    """
+    Replay the template used to generate *filename*.
+
+    If the replayed reduction differs from the original, the differences are
+    displayed, and the new version saved into /tmp/filename.
+
+    Raises *RuntimeError* if the files differ.
+    """
+
+    # Here's how the export file is constructed by the reductus client.
+    # This snippet is from reflweb/static/editor.js in the function
+    # webreduce.editor.export_data
+    """
+    var header = {
+        template_data: {
+            template: params.template,
+            node: params.node,
+            terminal: params.terminal,
+            server_git_hash: result.server_git_hash,
+            server_mtime: new Date((result.server_mtime || 0.0) * 1000).toISOString()
+        }
+    };
+    webreduce.download('# ' + JSON.stringify(header).slice(1,-1) + '\n' + result.values.join('\n\n'), filename);
+    """
+
+    # Load the template and the target output
+    with open(filename) as fid:
+        first_line = fid.readline()
+        template_data = json.loads(TEMPLATE.sub('{', first_line))
+        old_content = fid.read()
+
+    # Show the template
+    #print(json.dumps(template_data['template'], indent=2))
+
+    # Make sure instrument is available
+    template_module = template_data['template']['modules'][0]['module']
+    instrument_id = template_module.split('.')[1]
+    load_instrument(instrument_id)
+
+    # run the template
+    template = Template(**template_data['template'])
+    # extract module 'refl' from ncnr.refl.module into module id
+    target = template_data['node'], template_data['terminal']
+    template_config = {}
+    retval = process_template(template, template_config, target=target)
+    export = retval.get_export()
+    new_content = '\n\n'.join(export['values'])
+
+    has_diff = show_diff(old_content, new_content, "old", "new")
+    if has_diff:
+        # Save the new output into athe temp directory so we can easily update
+        # the regression tests
+        new_path = os.path.join('/tmp', os.path.basename(filename))
+        with open(new_path, 'wb') as fid:
+            fid.write(first_line)
+            fid.write(new_content)
+        raise RuntimeError("File replay for %r differs; new file stored in %r"
+                           % (filename, new_path))
+
+
+def main():
+    # type: (str) -> None
+    """
+    Run a regression test using the first command line as the target file.
+    """
+    set_test_cache()
+    load.DATA_SOURCES = config.data_sources
+
+    if len(sys.argv) < 2:
+        print("usage: python -m dataflow.show datafile")
+        sys.exit()
+    try:
+        replay_file(sys.argv[1])
+        sys.exit(0)
+    except Exception as exc:
+        print(str(exc), file=sys.stderr)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
