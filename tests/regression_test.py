29d52b1f9a42b54a10853c32a47cb002031a514b
Paul Kienzle <pkienzle@nist.gov>
2016-12-18 23:33:54 -0500

    add example regression test

---
+++ tests/regression_test.py
@@ -0,0 +1,25 @@
+from os import listdir
+from os.path import isfile, join as joinpath, dirname, abspath
+
+import regression
+from reflweb import config
+from dataflow.cache import set_test_cache
+from dataflow.modules import load
+
+def regression_test():
+    """Regressions tests from tests/regression_files, suitable for nosetests"""
+    set_test_cache()
+    load.DATA_SOURCES = config.data_sources
+    path = abspath(joinpath(dirname(__file__), 'regression_files'))
+
+    data_files = [p for f in listdir(path)
+                  for p in [joinpath(path, f)]
+                  if isfile(p)]
+    for file in data_files:
+        test = lambda: regression.replay_file(file)
+        test.description = "Regression test on %s"%file
+        yield test
+
+if __name__ == "__main__":
+    for test in regression_test():
+        test()
