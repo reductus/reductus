79ff23e18e13b37a8a916c0d41742b91c2d1a188
Paul Kienzle <pkienzle@nist.gov>
2017-05-03 22:14:12 -0400

    fallback to test cache if redis isn't available

--- dataflow/cache.py
+++ dataflow/cache.py
@@ -20,11 +20,11 @@ import time
 import tempfile
 
 def memory_cache():
-    import fakeredis
+    from . import fakeredis
     return fakeredis.MemoryCache()
-    
+
 def file_cache(cachedir="~/.reductus/cache"):
-    import fakeredis
+    from . import fakeredis
     return fakeredis.FileBasedCache(cachedir=cachedir)
 
 # port 6379 is the default port value for the python redis connection
@@ -39,33 +39,23 @@ def redis_connect(host="localhost", port=6379, maxmemory=4.0, **kwargs):
     import redis  # lazy import so that redis need not be available
 
     # ensure redis is running, at least if we are not on a windows box
-    redis_connected = False
     try:
         cache = redis.Redis(host=host, port=port, **kwargs)
         # first, check to see if it is already running:
         cache.ping()
-        redis_connected = True
     except redis.exceptions.ConnectionError:
         # if it's not running, and this is a platform on which we can start it:
-        if host == "localhost" and not sys.platform=='win32':
+        if host == "localhost" and not sys.platform == 'win32':
             subprocess.Popen(["redis-server"], stdout=open(os.devnull, "w"), stderr=subprocess.STDOUT)
             time.sleep(10)
             cache = redis.Redis(host=host, port=port, **kwargs)
-            try:
-                cache.ping()
-                redis_connected = True
-            except redis.exceptions.ConnectionError as exc: 
-                # if it's still not running, bail out and run the memory cache
-                warning = "Redis connection failed with:\n\t"
-                warning += str(exc)
-                warning += "\nFalling back to in-memory cache."
-                warnings.warn(warning)
-                cache = memory_cache()
-    
-    if redis_connected:
-        # set the memory settings for already-running Redis:
-        cache.config_set("maxmemory", "%d" % (int(maxmemory*2**30),))
-        cache.config_set("maxmemory-policy", "allkeys-lru")
+            cache.ping()
+        else:
+            raise
+
+    # set the memory settings for already-running Redis:
+    cache.config_set("maxmemory", "%d" % (int(maxmemory*2**30),))
+    cache.config_set("maxmemory-policy", "allkeys-lru")
 
     return cache
 
@@ -77,16 +67,21 @@ class CacheManager:
         self._cache = None
         self._file_cache = None
         self._redis_kwargs = None
+
     def _connect(self):
-        if self._cache is None:
-            if self._redis_kwargs is not None:
+        if self._cache is None and self._redis_kwargs is not None:
+            try:
                 self._cache = redis_connect(**self._redis_kwargs)
-            else:
-                self._cache = memory_cache()
+                return
+            except Exception as exc:
+                warning = "Redis connection failed with:\n\t" + str(exc)
+                warning += "\nFalling back to in-memory cache."
+                warnigns.warn(warning)
+        self.set_test_cache()
 
     def set_test_cache(self):
-        self._cache = memory_cache()
         cachedir = os.path.join(tempfile.gettempdir(), "reductus_test")
+        self._cache = memory_cache()
         self._file_cache = file_cache(cachedir=cachedir)
 
     def use_redis(self, **kwargs):
@@ -101,12 +96,14 @@ class CacheManager:
         if self._cache is not None:
             raise RuntimeError("call use_redis() before cache is first used")
         self._redis_kwargs = kwargs
+
     def get_cache(self):
         """
         Connect to the key-value cache.
         """
         self._connect()
         return self._cache
+
     def get_file_cache(self):
         self._connect()
         return self._file_cache if self._file_cache else self._cache
