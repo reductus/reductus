a80dcaf4c6910dcf038b458cdf4598cc32bb940d
bbm <brian.maranville@nist.gov>
2017-12-15 10:34:36 -0500

    starter script for flask with py3

---
+++ reflweb/start_flask_many_py3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for (( c=0; c<$2; c++))
+do
+    p=$(($1+$c));
+    uwsgi --socket "127.0.0.1:$p" --manage-script-name --mount /=server_flask:app --plugins-dir /usr/lib/uwsgi/plugins/ --plugin python3 -d /dev/null
+done
