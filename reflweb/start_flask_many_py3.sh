6c5c47864eeea32304a2af39b80d2b50b7ca2987
bbm <brian.maranville@nist.gov>
2018-01-12 17:44:24 -0500

    set umask to match user when starting uwsgi

--- reflweb/start_flask_many_py3.sh
+++ reflweb/start_flask_many_py3.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+UMASK="$(umask)"
+
 for (( c=0; c<$2; c++))
 do
     p=$(($1+$c));
-    uwsgi --socket "127.0.0.1:$p" --manage-script-name --mount /=server_flask:app --plugins-dir /usr/lib/uwsgi/plugins/ --plugin python3 -d /dev/null
+    uwsgi --umask "$UMASK" --socket "127.0.0.1:$p" --manage-script-name --mount /=server_flask:app --plugins-dir /usr/lib/uwsgi/plugins/ --plugin python3 -d /dev/null
 done
+
