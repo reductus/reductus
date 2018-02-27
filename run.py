a66ddb5d432e4ee75466ecbcb85d9f97c4060e5c
bbm <brian.maranville@nist.gov>
2018-02-27 15:25:35 -0500

    add flag to expose service on all network interfaces

--- run.py
+++ run.py
@@ -4,11 +4,13 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--headless', action='store_true', help='do not automatically load client in browser')
+    parser.add_argument('--external', action='store_true', help='listen on all interfaces, including external (local connections only if not set)')
     parser.add_argument('-p', '--port', default=8002, type=int, help='port on which to start the server')
     args = parser.parse_args()
     from reflweb.server_flask import app
     if not args.headless:
         import webbrowser
         webbrowser.open("http://localhost:%d" % (args.port))
-    app.run(port=args.port)
+    host = '0.0.0.0' if args.external else None
+    app.run(port=args.port, host=host)
     
