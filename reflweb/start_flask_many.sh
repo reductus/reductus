8fc98d73bc1886a792c54a455b6dcd3a1c2dc168
bbm <brian.maranville@nist.gov>
2018-02-27 15:28:17 -0500

    merge changes of python3 launcher script with python2 version

--- reflweb/start_flask_many.sh
+++ reflweb/start_flask_many.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+UMASK="$(umask)"
+killall -q uwsgi
+
 for (( c=0; c<$2; c++))
 do
     p=$(($1+$c));
-    uwsgi --socket "127.0.0.1:$p" --manage-script-name --mount /=server_flask:app --plugins-dir /usr/lib/uwsgi/plugins/ --plugin python -d /dev/null
+    uwsgi --umask "$UMASK" --socket "127.0.0.1:$p" --manage-script-name --mount /=server_flask:app --plugins-dir /usr/lib/uwsgi/plugins/ --plugin python -d /dev/null
 done
