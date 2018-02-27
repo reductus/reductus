5c3a26dffb1ecb7420e4ca56409b7fb198edffae
bbm <brian.maranville@nist.gov>
2018-02-27 11:54:27 -0500

    removing unused files, from hug server and a utility for preloading files

--- reflweb/start_hug_many.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-UWSGI_BIN=/home/bbm/.local/bin/uwsgi
-
-for (( c=0; c<$2; c++))
-do
-    p=$(($1+$c));
-    $UWSGI_BIN --socket "127.0.0.1:$p" --wsgi-file server_hug.py --callable __hug_wsgi__  -d /dev/null
-done
