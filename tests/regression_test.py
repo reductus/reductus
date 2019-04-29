338bc021c884f557fbb5f6ce096403a28c6f95bc
Paul Kienzle <pkienzle@nist.gov>
2019-04-29 13:08:15 -0400

    add pytest support for regression tests

--- tests/regression_test.py
+++ tests/regression_test.py
@@ -1,25 +1,46 @@
+from __future__ import print_function
+
 from os import listdir
-from os.path import isfile, join as joinpath, dirname, abspath
+from os.path import isfile, join as joinpath, dirname, abspath, exists
 
 import regression
 from reflweb import default_config as config
 from dataflow.cache import set_test_cache
 from dataflow import fetch
 
-def regression_test():
-    """Regressions tests from tests/regression_files, suitable for nosetests"""
+try:
+    import pytest
+    parametrize = pytest.mark.parametrize
+except ImportError:
+    # If pytest not installed then do simplet test suite which stops at the
+    # first regression failure
+    def parametrize(arg_names, values):
+        def wrapper(function):
+            def combined_tests():
+                for test in values:
+                    function(*test)
+            return combined_tests
+        return wrapper
+
+# Prefer not to scan regression directory on module load, but pytest doesn't
+# seem to have an interface for building tests on demand.
+REGRESSION_PATH = abspath(joinpath(dirname(__file__), 'regression_files'))
+
+def get_regression_files():
+    if exists(REGRESSION_PATH):
+        data_files = (f for f in listdir(REGRESSION_PATH)
+                    if isfile(joinpath(REGRESSION_PATH, f)))
+    else:
+        data_files = ()
+    return data_files
+
+@parametrize("filename", get_regression_files())
+def test_regression(filename):
     set_test_cache()
     fetch.DATA_SOURCES = config.data_sources
-    path = abspath(joinpath(dirname(__file__), 'regression_files'))
-
-    data_files = [p for f in listdir(path)
-                  for p in [joinpath(path, f)]
-                  if isfile(p)]
-    for file in data_files:
-        test = lambda: regression.replay_file(file)
-        test.description = "Regression test on %s"%file
-        yield test
+    #description = "Regression test on %s"%path
+    path = joinpath(REGRESSION_PATH, filename)
+    regression.replay_file(path)
 
 if __name__ == "__main__":
-    for test in regression_test():
-        test()
+    test_regression()
