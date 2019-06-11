01885c4ba55e079defbf46a4fbaafa40b8486278
bbm <brian.maranville@nist.gov>
2019-06-10 22:58:32 -0400

    aggregate for import of instruments

---
+++ reflweb/static/js/webreduce/instruments/index.js
@@ -0,0 +1,13 @@
+export const instruments = {};
+
+import dcs from './ncnr.dcs.js';
+import ospec from './ncnr.ospec.js';
+import refl from './ncnr.refl.js';
+import sans from './ncnr.sans.js';
+import tas from './ncnr.tas.js';
+
+instruments['ncnr.dcs'] = dcs;
+instruments['ncnr.ospec'] = ospec;
+instruments['ncnr.refl'] = refl;
+instruments['ncnr.sans'] = sans;
+instruments['ncnr.tas'] = tas;
