allow normalization by monitor when monitor counts are zero; add regression test with monitor=0; fix norm by power and add norm by roi

reflred/candor.py
@@ -76,14 +76,13 @@ class Candor(ReflData):
 
         # Counts
         # Load counts early so we can tell whether channels are axis 1 or 2
-        counts = data_as(das, 'areaDetector/counts', '')
+        counts = data_as(das, 'areaDetector/counts', '', dtype='d')
         if counts is None or counts.size == 0:
             raise ValueError(f"Candor file '{self.path}' has no area detector data.")
 
         channels_at_end = (counts.shape[2] == NUM_CHANNELS)
         if channels_at_end:
             counts = np.swapaxes(counts, 1, 2)
-        counts = np.asarray(counts, 'd') # convert data to float
         self.detector.counts = counts
         self.detector.counts_variance = counts.copy()
         self.detector.dims = counts.shape[1:]

reflred/nexusref.py
@@ -25,7 +25,7 @@ TRAJECTORY_INTENTS = {
 WAVELENGTH = 4.
 WAVELENGTH_DISPERSION = 0.015
 
-def data_as(group, fieldname, units, rep=None, NA=None):
+def data_as(group, fieldname, units, rep=None, NA=None, dtype=None):
     """
     Return value of field in the desired units.
     """
@@ -35,6 +35,8 @@ def data_as(group, fieldname, units, rep=None, NA=None):
     units_in = _s(field.attrs.get('units', ''))
     converter = unit.Converter(units_in)
     value = converter(field[()], units)
+    if dtype is not None:
+        value = np.asarray(value, dtype=dtype)
     if rep is not None:
         if np.isscalar(value) or len(value) == 1:
             return np.repeat(value, rep, axis=0)
@@ -151,11 +153,29 @@ def nexus_common(self, entry, entryname, filename):
     monitor_device = entry.get('control/monitor', {})
     self.monitor.deadtime = data_as(monitor_device, 'dead_time', 'us')
     self.monitor.deadtime_error = data_as(monitor_device, 'dead_time_error', 'us')
-    self.monitor.base = str_data(das, 'counter/countAgainst')
+    base = str_data(das, 'counter/countAgainst').lower()
+    # NICE stores TIME, MONITOR, ROI, TIME_MONITOR, TIME_ROI, etc.
+    if "monitor" in base:
+        base = "monitor"
+    elif "time" in base:
+        base = "time"
+    elif "roi" in base:
+        base = "roi"
+    else:
+        base = "none"
+
     self.monitor.time_step = 0.001  # assume 1 ms accuracy on reported clock
-    self.monitor.counts = np.asarray(data_as(das, 'counter/liveMonitor', '', rep=n), 'd')
+    self.monitor.counts = data_as(das, 'counter/liveMonitor', '', rep=n, dtype='d')
     self.monitor.counts_variance = self.monitor.counts.copy()
     self.monitor.count_time = data_as(das, 'counter/liveTime', 's', rep=n)
+    self.monitor.roi_counts = data_as(das, 'counter/liveROI', '', rep=n, dtype='d')
+    self.monitor.roi_variance = self.monitor.roi_counts.copy()
+    self.monitor.roi_variance = self.monitor.roi_counts.copy()
+    self.monitor.source_power = data_as(das,
+        'reactorPower/reactorPowerThermal/average_value', 'MW', rep=n, dtype='d')
+    self.monitor.source_power_variance = data_as(das, 
+        'reactorPower/reactorPowerThermal/average_value_error', 'MW', rep=n, dtype='d')
+    self.monitor.source_power_units = "MW"
 
     # NG7 monitor saturation is stored in control/countrate_correction
     saturation_device = entry.get('control/countrate_correction', None)
@@ -326,7 +346,7 @@ class NCNRNeXusRefl(ReflData):
         self.detector.rotation = data_as(entry, 'instrument/detector/rotation', 'degree')
 
         # Counts
-        self.detector.counts = np.asarray(data_as(das, 'counter/liveROI', ''), 'd')
+        self.detector.counts = data_as(das, 'counter/liveROI', '', dtype='d')
         self.detector.counts_variance = self.detector.counts.copy()
         self.detector.dims = self.detector.counts.shape[1:]
 

reflred/ng7psd.py
@@ -125,7 +125,7 @@ class NG7PSD(PSDData):
 
         # Load data from linear detector.  Note that counts/liveROI may not
         # match if counts/roiAgainst is against a different detector.
-        self.detector.counts = np.asarray(data_as(das, 'linearDetector/counts', ''), 'd')
+        self.detector.counts = data_as(das, 'linearDetector/counts', '', dtype='d')
         #print("detector shape", self.detector.counts.shape)
         self.detector.counts_variance = self.detector.counts.copy()
         self.detector.dims = self.detector.counts.shape[1:]

reflred/refldata.py
@@ -511,27 +511,20 @@ class Monitor(Group):
         user may generate a counts vector, for example by estimating
         the count rate by other means, in order to combine data
         measured by time with data measured by monitor when the
-        monitor values are otherwise unreliable.  Variance is assumed
-        to be the number of counts, after any necessary rebinning.
+        monitor values are otherwise unreliable.
+    counts_variance (n x k counts)
+        Variance is set to the number of counts but scaled during
+        monitor saturation and deadtime corrections.
+    roi_counts (n x k counts)
+        Count against a region of interest (ROI) on the detector.
+        **TODO**: ROI is **not** scaled during detector deadtime corrections,
+        and there is no correction for detector efficiency.
+    roi_variance (n x k counts)
+        Variance is to be the number of counts.
     count_time (n seconds)
         Duration of the measurement.  For scanning instruments, there is
         a separate duration for each measurement.  For TOF, this is a
         single value equal to the duration of the entire measurement.
-    source_power (n source_power_units)
-        The source power for each measurement.  For situations when the
-        monitor cannot be trusted (which can happen from time to time on
-        some instruments), we can use the number of protons incident on
-        the target (proton charge) or the energy of the source (reactor
-        power integrated over the duration of each measurement) as a proxy
-        for the monitor.  So long as the we normalize both the slit
-        measurement and the reflectivity measurement by the power, this
-        should give us a reasonable estimate of the reflectivity.  If
-        the information is available, this will be a better proxy for
-        monitor than measurement duration.
-    base ('time' | 'monitor' | 'power')
-        The measurement rate basis which should be used to normalize
-        the data.  This is initialized by the file loader, but may
-        be overridden during reduction.
     time_step (seconds)
         The count_time timer has a reporting unit, e.g. second, or
         millisecond, or in the case of NCNR ICP files, hundredths of
@@ -539,6 +532,21 @@ class Monitor(Group):
         is assumed to be uniform over the time_step, centered on
         the reported time, with a gaussian approximation of uncertainty
         being sqrt(time_step/12).
+    source_power (n source_power_units)
+        The average source power for each measurement. For situations when
+        the monitor cannot be trusted (which can happen from time to time on
+        some instruments), we can use the number of protons incident on
+        the target (proton charge) or the energy of the source (reactor
+        power integrated over the duration of each measurement) as a proxy
+        for the monitor.
+    source_power_units ('coulombs/s' | 'megawatts')
+        Units for source power.
+    source_power_variance (n source_power_units)
+        Variance in the measured source power
+    base ('time' | 'monitor' | 'roi' | 'power')
+        The measurement rate basis which should be used to normalize
+        the data.  This is initialized by the file loader, but may
+        be overridden during reduction.
     start_time (n seconds)
         For scanning instruments the start of each measurement relative
         to start of the scan.  Note that this is not simply sum of the
@@ -558,10 +566,6 @@ class Monitor(Group):
         ratio used to normalize data on some instruments.
     time_of_flight (k+1 millisecond)
         Time boundaries for the time-of-flight measurement
-    source_power_units ('coulombs' | 'megawatthours')
-        Units for source power.
-    source_power_variance (n source_power_units)
-        Variance in the measured source power
 
     The corrected monitor counts field will start as None, but may be
     set by a dead time correction, which scales the monitor according
@@ -573,6 +577,8 @@ class Monitor(Group):
     sampled_fraction = None
     counts = None
     counts_variance = None
+    roi_counts = None
+    roi_variance = None
     start_time = None
     count_time = None
     time_step = 1 # Default to nearest second
@@ -584,6 +590,7 @@ class Monitor(Group):
     saturation = None
     columns = {
         "counts": {"units": "counts", "variance": "counts_variance"},
+        "roi_counts": {"units": "counts", "variance": "roi_variance"},
         "count_time": {"units": "seconds"}
     }
     deadtime = None
@@ -1183,7 +1190,6 @@ class ReflData(Group):
         }
 
     def get_plottable(self):
-        print("=======> plottable")
         # Note: subclass this for non-traditional reflectometry measurements
         columns = self.columns # {name: {label: str, units: str, errorbars: str}}
         data_arrays = [
@@ -1191,13 +1197,16 @@ class ReflData(Group):
             else get_item_from_path(self, p)
             for p, v in columns.items()]
         data_arrays = [np.resize(d, self.points).tolist() for d in data_arrays]
-        datas = dict([(c, {"values": d}) for c,d in zip(columns.keys(), data_arrays)])
+        datas = {c: {"values": d} for c, d in zip(columns.keys(), data_arrays)}
         # add errorbars:
         for k in columns.keys():
             if 'errorbars' in columns[k]:
                 #print('errorbars found for column %s' % (k,))
                 errorbars = get_item_from_path(self, columns[k]['errorbars'])
-                datas[k]["errorbars"] = errorbars.tolist()
+                if errorbars is not None:
+                    datas[k]["errorbars"] = errorbars.tolist()
+                else:
+                    print(f"===> missing errorbars {columns[k]['errorbars']} for {k}")
         name = getattr(self, "name", "default_name")
         entry = getattr(self, "entry", "default_entry")
         series = [{"label": "%s:%s" % (name, entry)}]
@@ -1319,11 +1328,15 @@ class PSDData(ReflData):
         pass
 
 def get_item_from_path(obj, path):
-    result = obj
-    keylist = path.split("/")
-    while len(keylist) > 0:
-        result = getattr(result, keylist.pop(0), {})
-    return result
+    """
+    Fetch *obj.a.b.c* from path *"a/b/c"*.
+
+    Returns None if path does not exist.
+    """
+    *head, tail = path.split("/")
+    for key in head:
+        obj = getattr(obj, key, {})
+    return getattr(obj, tail, None)
 
 def _write_key_value(fid, key, value):
     value_str = json.dumps(value, cls=NumpyEncoder)

reflred/scale.py
@@ -39,37 +39,51 @@ def estimate_attenuation(datasets):
     index = np.sort([d.angular_resolution[0] for d in datasets])
 
 
-NORMALIZE_OPTIONS = 'auto|monitor|time|power|none'
+NORMALIZE_OPTIONS = 'auto|monitor|time|roi|power|none'
 def apply_norm(data, base='auto'):
     if base == 'auto':
-        if (data.monitor.counts > 0).all():
+        # We are ignoring counter.countAgainst since monitor is almost
+        # always the best choice for normalization.  Even when counting
+        # against time monitor normalization protects against flucuations
+        # in beam intensity.  Only reason not to use it is when the monitor
+        # is bad or missing.
+        if data.monitor.counts is not None and (data.monitor.counts > 0).any():
             base = 'monitor'
-        elif (data.monitor.count_time > 0).all():
+        elif data.monitor.count_time is not None and (data.monitor.count_time > 0).any():
             base = 'time'
-        elif data.monitor.source_power is not None:
-            base = 'power'
         else:
             base = 'none'
 
     C = data.detector.counts
     varC = data.detector.counts_variance
     if base == 'monitor':
-        assert (data.monitor.counts > 0).all(), "monitor counts are zero; can't normalize by monitor"
+        #assert (data.monitor.counts > 0).all(), "monitor counts are zero; can't normalize by monitor"
         M = data.monitor.counts
+        M[M == 0] = 1  # protect against zero counts in monitor
         varM = data.monitor.counts_variance
-        varM += (varM == 0)  # variance on zero counts is +/- 1
+        varM[varM == 0] = 1  # variance on zero counts is +/- 1
         units = 'monitor'
     elif base == 'time':
-        assert (data.monitor.count_time > 0).all(), "count time is zero; can't normalize by time"
+        #assert (data.monitor.count_time > 0).all(), "count time is zero; can't normalize by time"
         M = data.monitor.count_time
-        # Uniform distribution has variance of interval/12
+        M[M == 0] = data.monitor.time_step/2.  # protect against zero count time
+        # Uniform distribution has variance of interval width/12
         varM = data.monitor.time_step/12.
         units = 'second'
+    elif base == 'roi':
+        M = data.monitor.roi_counts
+        M[M == 0] = 1  # protect against zero counts in monitor
+        varM = data.monitor.roi_variance
+        varM[varM == 0] = 1  # variance on zero counts is +/- 1
+        units = 'roi count'
     elif base == 'power':
-        assert data.monitor.source_power is not None, "source power is unknown; can't normalize by power"
-        M = data.monitor.source_power
-        varM = 0
-        units = data.monitor.source_power_units
+        if data.monitor.source_power is None:
+            raise ValueError("source power is unknown; can't normalize by power")
+        # Power is in megawatts, not megawatt-hours, so scale by time.
+        time = data.monitor.count_time/3600.
+        M = data.monitor.source_power * time
+        varM = data.monitor.source_power_variance * time
+        units = data.monitor.source_power_units + "-hour"
     elif base == 'none':
         M = 1
         varM = 0