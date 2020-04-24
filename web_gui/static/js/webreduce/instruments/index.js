5e39915bf20b215057894ee34a65e10ee4dcffce
Brian Ben Maranville <brian.maranville@nist.gov>
2020-04-23 22:53:21 -0400

    bringing usans and vsans to module form

--- web_gui/static/js/webreduce/instruments/index.js
+++ web_gui/static/js/webreduce/instruments/index.js
@@ -4,12 +4,14 @@ import dcs from './ncnr.dcs.js';
 import ospec from './ncnr.ospec.js';
 import refl from './ncnr.refl.js';
 import sans from './ncnr.sans.js';
-//import tas from './ncnr.tas.js';
-//import vsans from './ncnr.vsans.js';
+import tas from './ncnr.tas.js';
+import vsans from './ncnr.vsans.js';
+import usans from './ncnr.usans.js';
 
 instruments['ncnr.dcs'] = dcs;
 instruments['ncnr.ospec'] = ospec;
 instruments['ncnr.refl'] = refl;
 instruments['ncnr.sans'] = sans;
-//instruments['ncnr.tas'] = tas;
-//instruments['ncnr.vsans'] = vsans;
+instruments['ncnr.tas'] = tas;
+instruments['ncnr.vsans'] = vsans;
+instruments['ncnr.usans'] = usans;
