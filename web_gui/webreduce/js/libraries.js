f5e084c3ca87be6816080ed917eedc48bd58cba0
Brian Ben Maranville <brian.maranville@nist.gov>
2020-07-14 10:07:48 -0400

    move all imports to libraries.js

--- web_gui/webreduce/js/libraries.js
+++ web_gui/webreduce/js/libraries.js
@@ -1,3 +1,15 @@
+// side-effects... no esm for these yet
+//import 'https://www.unpkg.com/jquery-ui@1.12.1/ui/core.js';
+import 'https://code.jquery.com/jquery-1.12.4.min.js';
+import 'https://code.jquery.com/ui/1.12.1/jquery-ui.min.js';
+import 'https://cdn.jsdelivr.net/gh/vakata/jstree@3.3/dist/jstree.min.js';
+
+import 'https://cdn.jsdelivr.net/gh/gildas-lormeau/zip.js@master/WebContent/zip.js';
+import 'https://cdn.jsdelivr.net/gh/gildas-lormeau/zip.js@master/WebContent/deflate.js';
+import 'https://cdn.jsdelivr.net/gh/gildas-lormeau/zip.js@master/WebContent/inflate.js';
+import 'https://cdn.jsdelivr.net/gh/gildas-lormeau/zip.js@master/WebContent/z-worker.js';
+
+
 import { default as d3 } from "https://dev.jspm.io/d3@5";
 export { d3 };
 import { default as messagepack } from "https://dev.jspm.io/npm:messagepack@1.1";
