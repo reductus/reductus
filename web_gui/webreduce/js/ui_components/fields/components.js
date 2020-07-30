49290e5706fde1362fc7eb0f1c248ad371e92309
Brian Ben Maranville <brian.maranville@nist.gov>
2020-07-30 16:58:34 -0400

    linking to string field renderer

--- web_gui/webreduce/js/ui_components/fields/components.js
+++ web_gui/webreduce/js/ui_components/fields/components.js
@@ -1,5 +1,6 @@
 import { FileinfoUi } from './fileinfo.js';
 import { FloatUi, IntUi } from './number.js';
+import { StringUi } from './string.js';
 import { BoolUi } from './boolean.js';
 import { OptUi } from './opt.js';
 import { ScaleUi } from './scale.js';
@@ -9,6 +10,7 @@ export const Components = {
   FileinfoUi,
   FloatUi,
   IntUi,
+  StringUi,
   BoolUi,
   OptUi,
   ScaleUi,
