f1a4ceeaecce12124811df566ca3cca46fada653
Brian Ben Maranville <brian.maranville@nist.gov>
2020-07-30 21:50:40 -0400

    switching plot_metadata to Vue fromd3

--- web_gui/webreduce/js/plotters/plot_metadata.js
+++ web_gui/webreduce/js/plotters/plot_metadata.js
@@ -1,19 +1,51 @@
-import { d3 } from '../libraries.js';
+import { Vue } from '../libraries.js';
+
+let template = `
+<div class="metadata-table" style="overflow-x:scroll;">
+  <table class="metadata">
+    <thead>
+      <tr>
+        <th v-for="col in cols" :key="col">{{col}}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr 
+        v-for="row in metadata" 
+        :key="JSON.stringify(row)"
+        class="metadata-row"
+        >
+        <td v-for="col in cols" :key="col">{{pretty(row[col])}}</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+`;
+
+const MetadataTable = {
+  name: "metadata-table",
+  data: () => ({
+    cols: [],
+    metadata: [],
+    precision: 5
+  }),
+  methods: {
+    pretty(value) {
+      if (value && value.toPrecision) {
+        return value.toPrecision(this.precision);
+      }
+      else {
+        return value;
+      }
+    }
+  },
+  template
+}
 
 export function show_plots_metadata(plotdata, plot_controls, target, old_plot) {
   var metadata = plotdata.values.map(function (v) { return v.values });
   var m0 = metadata[0] || {};
 
-  plot_controls.plotnumber.show = false;
-  plot_controls.axes.x.coord.show = false;
-  plot_controls.axes.x.transform.show = false;
-  plot_controls.axes.y.coord.show = false;
-  plot_controls.axes.y.transform.show = false;
-  plot_controls.settings.errorbars.show = false;
-  plot_controls.settings.points.show = false;
-  plot_controls.settings.line.show = false;
-  plot_controls.settings.grid.show = false;
-  plot_controls.colormap.show = false;
+  plot_controls.updateShow([]);
 
   var colset = new Set(Object.keys(m0));
   for (var nm of metadata.slice(1)) {
@@ -24,42 +56,54 @@ export function show_plots_metadata(plotdata, plot_controls, target, old_plot) {
     }
   }
   var cols = Array.from(colset);
-  d3.select(target).selectAll("svg, div").remove();
-  d3.select(target).classed("plot", false);
-  let metadata_table = d3.select(target).append("div").append("table").classed("metadata", true)
-  metadata_table
-    .append("thead").append("tr")
-    .selectAll(".colHeader")
-    .data(cols).enter()
-    .append("th")
-    .classed("colHeader", true)
-    .text(function (d) { return String(d) })
+  while (target.firstChild) {
+    // clean out the target
+    target.removeChild(target.firstChild);
+  }
+  target.classList.remove("plot");
 
-  metadata_table
-    .append("tbody")
-    .selectAll(".metadata-row")
-    .data(metadata).enter()
-    .append("tr")
-    .classed("metadata-row", true)
-    .on("click", function () {
-      metadata_table.selectAll(".metadata-row")
-        .classed("active", false);
-      d3.select(this).classed("active", true);
+  let MetadataTableClass = Vue.extend(MetadataTable);
+  let metadata_table = new MetadataTableClass({
+    data: () => ({
+      cols,
+      metadata
     })
-    .each(function (d) {
-      let row = d3.select(this);
-      cols.forEach(function (c) {
-        row.append("td").append("pre")
-          //.attr("contenteditable", true)
-          //.on("input", function(dd, ii) { 
-          //  let new_text = this.innerText;
-          //  let old_text = String(d[c]);
-          //  let dirty = (old_text != new_text);
-          //  d3.select(this.parentNode).classed("dirty", dirty);
-          //})
-          .text(String(d[c]));
-      })
-    });
+  }).$mount(target);
+  // let metadata_table = d3.select(target).append("div").append("table").classed("metadata", true)
+  // metadata_table
+  //   .append("thead").append("tr")
+  //   .selectAll(".colHeader")
+  //   .data(cols).enter()
+  //   .append("th")
+  //   .classed("colHeader", true)
+  //   .text(function (d) { return String(d) })
+
+  // metadata_table
+  //   .append("tbody")
+  //   .selectAll(".metadata-row")
+  //   .data(metadata).enter()
+  //   .append("tr")
+  //   .classed("metadata-row", true)
+  //   .on("click", function () {
+  //     metadata_table.selectAll(".metadata-row")
+  //       .classed("active", false);
+  //     d3.select(this).classed("active", true);
+  //   })
+  //   .each(function (d) {
+  //     let row = d3.select(this);
+  //     cols.forEach(function (c) {
+  //       row.append("td").append("pre")
+  //         //.attr("contenteditable", true)
+  //         //.on("input", function(dd, ii) { 
+  //         //  let new_text = this.innerText;
+  //         //  let old_text = String(d[c]);
+  //         //  let dirty = (old_text != new_text);
+  //         //  d3.select(this.parentNode).classed("dirty", dirty);
+  //         //})
+  //         .text(String(d[c]));
+  //     })
+  //   });
 
   return metadata_table
 }
+
