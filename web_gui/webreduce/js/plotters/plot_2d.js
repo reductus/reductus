415d42e5c67bd00a02fd09bb067519d9c0947458
Brian Ben Maranville <brian.maranville@nist.gov>
2020-08-14 17:07:19 -0400

    fix 2d multi plot so that it redraws every time a module is clicked

--- web_gui/webreduce/js/plotters/plot_2d.js
+++ web_gui/webreduce/js/plotters/plot_2d.js
@@ -14,62 +14,61 @@ export async function show_plots_2d_multi(plotdata, plot_controls, target, old_p
 
     mychart = new heatChartMultiMasked(options, d3);
     data = values[0];
-    
-    if (options.ztransform) {
-      plot_controls.axes.z.transform.value = options.ztransform;
-    } else {
-      options.ztransform = plot_controls.axes.z.transform.value;
-    }
-
-    plot_controls.updateShow([
-      "plotnumber",
-      "axes/z",
-      "settings/grid",
-      "colormap",
-      "export_data"
-    ])
-
-    plot_controls.colormap.options = colormap_names;
-
-    plot_controls.settingChange = function (setting_name, value) {
-      var o = mychart.options();
-      let opt_name = 'show_' + setting_name;
-      o[opt_name] = value;
-      mychart.options(o).update();
-    }
-
-    plot_controls.transformChange = function (axis, transform) {
-      mychart[axis + 'transform'](transform);
-    }
-    plot_controls.colormapChange = function (colormap_name) {
-      var new_colormap = get_colormap(colormap_name, d3);
-      mychart.colormap(new_colormap).redrawImage();
-      mychart.colorbar.update();
-    }
-    plot_controls.plotNumberChange = function (plotnum) {
-      var plotnum = plotnum || 0;
-      data = values[plotnum];
-      plot_controls.$emit("title", data.title || "");
-      data.ztransform = plot_controls.axes.z.transform.value;
-      var aspect_ratio = null;
-      if ((((data.options || {}).fixedAspect || {}).fixAspect || null) == true) {
-        aspect_ratio = ((data.options || {}).fixedAspect || {}).aspectRatio || null;
-      }
-      mychart.options(data.options);
-      mychart
-        .aspect_ratio(aspect_ratio)
-        .xlabel(data.xlabel)
-        .ylabel(data.ylabel);
-      var new_colormap = get_colormap(plot_controls.colormap.value, d3);
-      mychart.colormap(new_colormap);
-      mychart.source_data(data.datasets);
-      mychart.zoomScroll(true);
-      mychart.ztransform(plot_controls.axes.z.transform.value);
-    }
-    
     await plot_controls.$nextTick();
     d3.selectAll([target]).data([values[0].datasets]).call(mychart);
     app.callbacks.resize_center = function () { mychart.autofit() };
+  }
+    
+  if (options.ztransform) {
+    plot_controls.axes.z.transform.value = options.ztransform;
+  } else {
+    options.ztransform = plot_controls.axes.z.transform.value;
+  }
+
+  plot_controls.updateShow([
+    "plotnumber",
+    "axes/z",
+    "settings/grid",
+    "colormap",
+    "export_data"
+  ])
+
+  plot_controls.colormap.options = colormap_names;
+
+  plot_controls.settingChange = function (setting_name, value) {
+    var o = mychart.options();
+    let opt_name = 'show_' + setting_name;
+    o[opt_name] = value;
+    mychart.options(o).update();
+  }
+
+  plot_controls.transformChange = function (axis, transform) {
+    mychart[axis + 'transform'](transform);
+  }
+  plot_controls.colormapChange = function (colormap_name) {
+    var new_colormap = get_colormap(colormap_name, d3);
+    mychart.colormap(new_colormap).redrawImage();
+    mychart.colorbar.update();
+  }
+  plot_controls.plotNumberChange = function (plotnum) {
+    var plotnum = plotnum || 0;
+    let data = values[plotnum];
+    plot_controls.$emit("title", data.title || "");
+    data.ztransform = plot_controls.axes.z.transform.value;
+    var aspect_ratio = null;
+    if ((((data.options || {}).fixedAspect || {}).fixAspect || null) == true) {
+      aspect_ratio = ((data.options || {}).fixedAspect || {}).aspectRatio || null;
+    }
+    mychart.options(data.options);
+    mychart
+      .aspect_ratio(aspect_ratio)
+      .xlabel(data.xlabel)
+      .ylabel(data.ylabel);
+    var new_colormap = get_colormap(plot_controls.colormap.value, d3);
+    mychart.colormap(new_colormap);
+    mychart.source_data(data.datasets);
+    mychart.zoomScroll(true);
+    mychart.ztransform(plot_controls.axes.z.transform.value);
   }
 
   plot_controls.plotnumber.max = plotdata.values.length - 1;
