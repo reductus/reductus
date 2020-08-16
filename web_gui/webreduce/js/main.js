8bbf0b0cfdadc1fbf86bcb58fba35ab1e7b365f0
Brian Ben Maranville <brian.maranville@nist.gov>
2020-08-16 18:18:27 -0400

    cleanup unused imports

--- web_gui/webreduce/js/main.js
+++ web_gui/webreduce/js/main.js
@@ -1,16 +1,8 @@
 "use strict";
 // SIDE-EFFECTS ONLY FOR NOW...
-//import * as webreduce from './webreduce';
-//const webreduce = {};
-//export {webreduce};
-//import {d3} from './libraries.js';
-// create a new global d3 object;
-//window.d3 = d3;
 import { extend } from './libraries.js';
 import { Split } from './libraries.js';
 import { zip } from './libraries.js';
-//import {jquery_ui} from './libraries.js';
-//window.jquery_ui = jquery_ui;
 import { editor } from './editor.js';
 import { server_api } from './server_api/api_msgpack.js';
 import { filebrowser } from './filebrowser.js';
