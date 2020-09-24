b7883862aabd4511b3ecfca42b34fa2b98ed6d85
Brian Ben Maranville <brian.maranville@nist.gov>
2020-09-24 11:27:31 -0400

    updating opt UI to show blank + default on the side for unspecified fields

--- web_gui/webreduce/js/ui_components/fields/opt.js
+++ web_gui/webreduce/js/ui_components/fields/opt.js
@@ -4,12 +4,16 @@ let template = `
     {{field.label}}
     <select
       :id="field.id"
-      :value="(value != null) ? value : field.default"
+      :value="value"
       @change="$emit('change', field.id, $event.target.value)"
     >
+      <option value="" v-if="value == null"></option>
       <option v-for="(c, i) in field.typeattr.choices" :value="c[1]">{{c[0]}}</option>
     </select>
   </label>
+  <span v-if="value == null">
+    <em>default: {{field.default}}</em>
+  </span>
 </div>
 `;
 
