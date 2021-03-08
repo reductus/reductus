a1e0f4b2d15abefbb807b32fe433f250eb533733
Brian Ben Maranville <brian.maranville@nist.gov>
2021-03-08 14:09:59 -0500

    only record users that actually run at least one calculation

--- provisioning/get_stats.sh
+++ provisioning/get_stats.sh
@@ -3,9 +3,13 @@
 # filter bots:
 # zgrep -h -i -vE "googlebot|bingbot|msnbot|slurp|mediapartners-google" /var/log/apache2/access.log* | awk '{ print $1 } '| sort | uniq | wc -l
 
-for i in `zgrep -h -i -vE "googlebot|bingbot|msnbot|slurp|mediapartners-google|yandex" /var/log/apache2/access.log* | awk '{ print $1 } '| sort | uniq `
+for i in `zgrep -h "calc_terminal" /var/log/apache2/access.log* | awk '{ print $1 } '| sort | uniq `
 do
+  if [[ $i == *":"* ]]; then
+    geoiplookup6 $i | head -1 | cut -d' ' -f 6-
+  else
     geoiplookup $i | head -1 | cut -d' ' -f 5-
+  fi
 done > 'countrynamelist.txt'
 
 cat countrynamelist.txt | sort | uniq | while read line
