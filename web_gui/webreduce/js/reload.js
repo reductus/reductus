5f9d71d28432e9a66e93f38d3635ce41dae4e25d
Brian Ben Maranville <brian.maranville@nist.gov>
2021-12-13 16:05:20 -0500

    add orso YAML parser

--- web_gui/webreduce/js/reload.js
+++ web_gui/webreduce/js/reload.js
@@ -1,9 +1,12 @@
+import { yaml_load } from './libraries.js';
+
 const signatures = [
   ['json', [123]], // just the { character
   ['column', [35,34,116,101,109,112,108,97,116,101,95,100,97,116,97,34,58]], // #"template_data":
   ['column', [35,32,34,116,101,109,112,108,97,116,101,95,100,97,116,97,34,58]], // # "template_data":
   ['hdf5', [137,72,68,70,13,10,26,10]],
-  ['png', [137,80,78,71,13,10,26,10]]
+  ['png', [137,80,78,71,13,10,26,10]],
+  ['orso', [35,32,35,32,79,82,83,79,32,114,101,102,108,101,99,116,105,118,105,116,121,32,100,97,116,97,32,102,105,108,101]]
 ];
 
 function get_type(contents) {
@@ -29,6 +32,22 @@ readers.column = function(contents, sig_length) {
   let trimmed = text.slice(0, first_return);
   trimmed = trimmed.replace(/^#\s?"template_data":/, "");
   return JSON.parse(trimmed);
+}
+
+readers.orso = function(contents) {
+  let text = new TextDecoder('utf-8').decode(contents);
+  let lines = text.split("\n");
+  if (/YAML encoding/.test(lines[0])) {
+    let raw_header = "";
+    for (let line of lines) {
+      if (/^# data_set/.test(line)) {
+        break
+      }
+      raw_header += line.replace(/^# /, '') + '\n';
+    }
+    let header = yaml_load(raw_header);
+    return header?.reduction?.software?.template_data;
+  }
 }
 
 export function reload(contents) {
