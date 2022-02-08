f8cb963de2f72245831174adef1e754c2b897691
Brian Ben Maranville <brian.maranville@nist.gov>
2022-02-08 14:32:19 -0500

    detect if mirror is defined in incident beam

--- reflred/xrdml.py
+++ reflred/xrdml.py
@@ -32,6 +32,7 @@ def parse_measurement_node(node, ns, context):
     # output['wavelength'] = (ka1 + kratio * ka2) / (kratio + 1.0)
 
     context['incidentRadius'] = float(node.find("./incidentBeamPath/radius", ns).text)
+    context['incidentMirrorPresent'] = node.find("./incidentBeamPath/xRayMirror", ns) is not None
     context['sourceLineWidth'] = float(node.find("./incidentBeamPath/xRayTube/focus/width", ns).text)
 
     divergence_slit = node.find("./incidentBeamPath/divergenceSlit", ns)
@@ -41,9 +42,9 @@ def parse_measurement_node(node, ns, context):
 
     context['diffractedRadius'] = float(node.find("./diffractedBeamPath/radius", ns).text)
     
-    detector = node.find("./diffractedBeamPath/detector", ns)
-    detectorTypeKey = next(k for k in detector.attrib if k.endswith('type'))
-    detectorType = detector.attrib[detectorTypeKey]
+    # detector = node.find("./diffractedBeamPath/detector", ns)
+    # detectorTypeKey = next(k for k in detector.attrib if k.endswith('type'))
+    # detectorType = detector.attrib[detectorTypeKey]
     
     receivingSlitHeight = None
     receiving_slit = node.find("./diffractedBeamPath/receivingSlit", ns)
