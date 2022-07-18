303acbab2c760aac6b3227f837b2f2c34b2fcff8
div099 <divija.katakam@gmail.com>
2022-07-18 15:34:15 -0400

    updating promise worker

--- web_gui/webreduce/js/server_api/api_webworker.js
+++ web_gui/webreduce/js/server_api/api_webworker.js
@@ -45,18 +45,18 @@ toWrap.forEach(function(method_name) {
 
 server_api.__init__ = function() {
   const worker = new Worker('worker.js');
-  var server_api.promiseWorker = new PromiseWorker('worker.js'); // creates new promise worker
-  const promiseWorker = new PromiseWorker('worker.js'); // GO OVER
+  const promiseWorker = new PromiseWorker('worker'); // GO OVER
+  server_api.promiseWorker = PromiseWorker('worker'); // creates new promise worker
 
 
   addButton.addEventListener("click", (event) => {
     let one = document.getElementById("firstInput").value;
     let two = document.getElementById("secondInput").value;
 
-    worker.postMessage([one, two]);
+    worker.postMessage([one, two]); // main js script sends a message to worker
   });
 
-  worker.onmessage = function(message) {
+  worker.onmessage = function(message) { // main js script recieves a message from worker
     document.getElementById("printSum").innerHTML = message.data;
 }}
 
