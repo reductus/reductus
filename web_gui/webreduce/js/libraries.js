95fb5d7e732f7fc6980e10ee1df2f9662577c5c6
Brian Ben Maranville <brian.maranville@nist.gov>
2023-03-10 09:28:33 -0500

    update libraries.js to move to cdn.jsdelivr and pin vue-material version

--- web_gui/webreduce/js/libraries.js
+++ web_gui/webreduce/js/libraries.js
@@ -1,4 +1,4 @@
-import 'https://unpkg.com/d3@5.16.0/dist/d3.min.js';
+import 'https://cdn.jsdelivr.net/npm/d3@5.16.0/dist/d3.min.js';
 let d3 = window.d3;
 export {d3};
 
@@ -9,7 +9,7 @@ export {default as Tree} from 'https://cdn.jsdelivr.net/gh/bmaranville/treejs@la
 //export {default as Tree} from './treejs/src/index.js';
 
 import { default as Vue } from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js';
-import { default as VueMaterial } from 'https://cdn.skypack.dev/vue-material@latest';
+import { default as VueMaterial } from 'https://cdn.skypack.dev/vue-material@1.0.0-beta-15';
 Vue.use(VueMaterial);
 //import { default as VueSimpleContextMenu } from 'https://cdn.skypack.dev/vue-simple-context-menu@^3.1.10';
 //Vue.use(VueSimpleContextMenu);
@@ -22,11 +22,11 @@ Vue.use(VueMaterial);
 export { default as vuedraggable } from 'https://cdn.skypack.dev/vuedraggable';
 export { Vue }
 
-import * as messagepack from "https://unpkg.com/messagepack@1.1.11/dist/messagepack.es.js";
+import * as messagepack from "https://cdn.jsdelivr.net/npm/messagepack@1.1.12/dist/messagepack.es.js";
 export { messagepack };
 // PouchDB can be replaced with idb from https://github.com/jakearchibald/idb 
 // since we are not using the sync features of pouch
-import * as idb from 'https://unpkg.com/idb?module';
+import * as idb from 'https://cdn.jsdelivr.net/npm/idb@7.0.2/build/index.js';
 export { idb };
 export {
     xyChart,
@@ -47,7 +47,10 @@ export {
     rectangleSelectPoints
 }  from 'https://cdn.jsdelivr.net/gh/usnistgov/d3-science@0.2.14/src/index.js';
 //}  from './d3-science/src/index.js';
-
 export {default as sha1} from "https://cdn.skypack.dev/sha1@1.1.1";
 export { default as Split } from "https://cdn.skypack.dev/split.js";
-export {default as json_patch} from "https://cdn.skypack.dev/json-patch-es6";
+export { load as yaml_load } from "https://cdn.jsdelivr.net/npm/js-yaml@latest/dist/js-yaml.mjs";
+
+import 'https://cdn.jsdelivr.net/npm/promise-worker@2.0.1/dist/promise-worker.js';
+const PromiseWorker = window.PromiseWorker;
+export { PromiseWorker };
