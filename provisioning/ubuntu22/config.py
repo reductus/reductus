ff9455aba4cda65740b3ed244964df3ab78305cf
bbm <brian.maranville@nist.gov>
2025-01-17 09:46:52 -0500

    adding provisioning instructions for new ubuntu22 hosts

---
+++ provisioning/ubuntu22/config.py
@@ -0,0 +1,25 @@
+#############################################################
+# rename or copy this file to config.py if you make changes #
+#############################################################
+
+# change this to your fully-qualified domain name to run a 
+# remote server.  The default value of localhost will
+# only allow connections from the same computer.
+#jsonrpc_servername = "h3.umd.edu"
+config = {
+
+    "cache": {
+        "engine": "diskcache",
+        "params": {"size_limit": int(4*2**30), "shards": 5},
+        "compression": True
+    },
+    "data_sources": [
+        {
+            "name": "ncnr",
+            "url": "https://www.ncnr.nist.gov/pub/",
+            "start_path": "ncnrdata",
+            "file_helper_url": "http://10.201.1.148/ncnrdata/listftpfiles_pg.php"
+        },
+    ],
+    "instruments": ["refl", "ospec", "sans", "dcs", "usans"],
+}
