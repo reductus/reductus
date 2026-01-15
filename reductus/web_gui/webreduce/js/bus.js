4126400d6705c72054d2ea1bda25b818a0648829
bbm <brian.maranville@nist.gov>
2026-01-15 15:36:39 -0500

    replace mitt with local async emitter

--- reductus/web_gui/webreduce/js/bus.js
+++ reductus/web_gui/webreduce/js/bus.js
@@ -1,3 +1,37 @@
-import mitt from 'mitt';
+// Custom async event emitter
+function createAsyncEmitter() {
+  const handlers = new Map();
 
-export const emitter = mitt();
+  return {
+    on(type, handler) {
+      if (!handlers.has(type)) {
+        handlers.set(type, []);
+      }
+      handlers.get(type).push(handler);
+    },
+
+    off(type, handler) {
+      if (!handlers.has(type)) return;
+      
+      if (handler) {
+        const list = handlers.get(type);
+        const index = list.indexOf(handler);
+        if (index > -1) {
+          list.splice(index, 1);
+        }
+      } else {
+        handlers.delete(type);
+      }
+    },
+
+    async emit(type, data) {
+      const list = handlers.get(type);
+      if (!list || list.length === 0) return;
+      
+      // Execute all handlers and wait for them to complete
+      await Promise.all(list.map(handler => handler(data)));
+    }
+  };
+}
+
+export const emitter = createAsyncEmitter();
