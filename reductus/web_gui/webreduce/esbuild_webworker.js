c4afdede603840128a48b6985379356ae963d9a7
bbm <brian.maranville@nist.gov>
2026-03-20 11:24:27 -0400

    use server_api alias (to override in webworker version)

--- reductus/web_gui/webreduce/esbuild_webworker.js
+++ reductus/web_gui/webreduce/esbuild_webworker.js
@@ -8,20 +8,37 @@ import { copyRecursiveSync } from './copy_recursive_sync.mjs';
 const __dirname = path.resolve();
 
 await esbuild.build({
-  entryPoints: ['js/main.js'],
+  entryPoints: ['index.html'],
   bundle: true,
   minify: true,
   sourcemap: true,
   define: { ENABLE_UPLOADS: 'true' },
   outdir: 'dist_webworker/js',
-  target: 'es2016',
+  target: 'es2020',
   alias: {
     // Use Vue's full build with template compiler instead of runtime-only
-    'vue': 'vue/dist/vue.esm-bundler.js'
+    'vue': 'vue/dist/vue.esm-bundler.js',
+    "server_api": "./js/server_api/api_webworker.js",
   },
   plugins: [
   ],
 }).catch(() => process.exit(1))
+
+await esbuild.build({
+  entryPoints: ['js/server_api/worker.js'],
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  outdir: 'dist_webworker',
+  target: 'es2020',
+  format: 'esm',
+  // alias: {
+  //   "comlink": "https://unpkg.com/comlink/dist/esm/comlink.mjs"
+  // },
+  plugins: [
+  ],
+}).catch(() => process.exit(1))
+
 
 await esbuild.build({
   entryPoints: ['css/index_prod.css'],
@@ -41,7 +58,7 @@ let mode = fs.constants.COPYFILE_FICLONE;
 copyRecursiveSync('index.html', 'dist_webworker/index.html', mode);
 copyRecursiveSync('favicon.ico', 'dist_webworker/favicon.ico', mode);
 copyRecursiveSync('img', 'dist_webworker/img', mode);
-copyRecursiveSync('js/server_api/worker.js', 'dist_webworker/worker.js', mode);
+// copyRecursiveSync('js/server_api/worker.js', 'dist_webworker/worker.js', mode);
 copyRecursiveSync('../../../dist', 'dist_webworker', mode)
 
 // generate a list of files ending in .whl in the dist folder and write to file
